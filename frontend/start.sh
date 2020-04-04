d5d2fcb0f362161f673cb13be1ec655f0be89b02
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-04 13:30:46 +0800

    update start.sh & Dockerfile

--- frontend/start.sh
+++ frontend/start.sh
@@ -4,16 +4,12 @@ echo "Building frontend image ..."
 
 docker build -t frontend .
 
-for container_id in $(docker ps -a | grep frontend-dev | awk '{print $1}')
-do
-    echo "Deleting former frontend-dev ..."
-    docker rm -f ${container_id}
-done
+echo "Deleting former frontend-dev ..."
+
+docker rm -f $(docker ps -a | grep frontend-dev | awk '{print $1}')
 
 echo "Starting frontend-dev ..."
+
 echo "Running on http://127.0.0.1:9000/"
 
-# docker run -it --name frontend-dev -p 9000:8080 -v $(pwd)/app:/app frontend bash -c "yarn && yarn serve"
-
-# lastline in Dockerfile is `CMD ["yarn", "serve"]`
 docker container run -it --name frontend-dev -p 9000:8080 frontend
