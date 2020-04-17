e87830edab883a8974e058c30b4a63095c72e93d
zsstrike <2439759713@qq.com>
2020-04-17 12:44:38 +0800

    finish course-app views

---
+++ frontend/course-app/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav-bar">
+    <p>This is NavBar component.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped>
+
+</style>>
