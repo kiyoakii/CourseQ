a47d7da91f4c9fbf7043057c38813f43716d665a
zsStrike <2439758713@qq.com>
2020-04-17 14:05:38 +0800

    finish course-app structure

---
+++ frontend/course-app/src/components/CourseAnnounce.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is CourseAnnounce view.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseAnnounce',
+};
+</script>
+
+<style>
+
+</style>
---
+++ frontend/course-app/src/components/TeacherInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is TeacherInfo view.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeacherInfo',
+};
+</script>
+
+<style>
+
+</style>
