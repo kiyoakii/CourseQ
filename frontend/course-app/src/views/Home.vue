fce1b12462dd2f351bc4dbcf22b5c66e86fa36c3
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-21 13:34:44 +0800

    finish Home.vue

--- frontend/course-app/src/views/Home.vue
+++ frontend/course-app/src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     This is Home view.
+    <course-info></course-info>
+    <teacher-info></teacher-info>
+    <course-announce></course-announce>
+    <course-assigns></course-assigns>
   </div>
 </template>
 
 <script>
+import CourseInfo from '@/components/CourseInfo.vue';
+import TeacherInfo from '@/components/TeacherInfo.vue';
+import CourseAnnounce from '@/components/CourseAnnounce.vue';
+import CourseAssigns from '@/components/CourseAssigns.vue';
+
 export default {
-  name: 'CourseInfo',
+  name: 'Home',
+  components: {
+    CourseInfo,
+    TeacherInfo,
+    CourseAnnounce,
+    CourseAssigns,
+  },
 };
 </script>
 
