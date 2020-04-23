b39d895d4286a8b83bbb71b4aed9df7e77df3c06
zsstrike <2439759713@qq.com>
2020-04-23 19:23:14 +0800

    finish NavBar

--- frontend/course-app/src/components/NavBar.vue
+++ frontend/course-app/src/components/NavBar.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="nav-bar">
-    <el-menu default-active="/" class="el-menu-demo" mode="horizontal" router>
+    <el-menu default-active="/" mode="horizontal" router>
       <el-menu-item index="/">课程名</el-menu-item>
       <el-menu-item index="/course-calendar">课程日历</el-menu-item>
       <el-menu-item index="/course-res">课程资源</el-menu-item>
-      <el-menu-item :index="$router.path"><a href="https://www.ele.me" target="_blank">答疑平台</a></el-menu-item>
+      <el-menu-item :index="$router.path"><el-link href="https://www.ele.me" target="_blank">答疑平台</el-link></el-menu-item>
       <el-submenu index="older">
         <template slot="title">过往学期</template>
-        <el-menu-item :index="$router.path"><a href="https://www.ele.me" target="_blank">过往学期1</a></el-menu-item>
-        <el-menu-item :index="$router.path"><a href="https://www.ele.me" target="_blank">过往学期2</a></el-menu-item>
-        <el-menu-item :index="$router.path"><a href="https://www.ele.me" target="_blank">过往学期3</a></el-menu-item>
+        <el-menu-item :index="$router.path">
+          <el-link href="https://www.ele.me" target="_blank" type="info" :underline="false" style="display:block;text-align:center;">过往学期1</el-link>
+        </el-menu-item>
+        <el-menu-item :index="$router.path">
+          <el-link href="https://www.ele.me" target="_blank" type="info" :underline="false" style="display:block;text-align:center;">过往学期2</el-link>
+        </el-menu-item>
+        <el-menu-item :index="$router.path">
+          <el-link href="https://www.ele.me" target="_blank" type="info" :underline="false" style="display:block;text-align:center;">过往学期3</el-link>
+        </el-menu-item>
       </el-submenu>
     </el-menu>
   </div>
@@ -18,15 +24,22 @@
 <script>
 export default {
   name: 'NavBar',
-  methods: {
-    handleRoute(key, keypath) {
-      console.log(key, keypath, this.$event);
-      // this.$router.push('/course-res');
-    },
-  },
 };
 </script>
 
 <style scoped>
+.el-menu {
+  display: flex;
+  justify-content: flex-end;
+}
 
-</style>>
+.el-submenu {
+  padding-right: 2em;
+}
+
+.el-submenu .el-menu-item{
+  display: flex;
+  justify-content: center;
+}
+
+</style>
