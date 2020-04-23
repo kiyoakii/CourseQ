8f1bf5b55b3ee42af4145372591453b551a05dbe
zsstrike <2439759713@qq.com>
2020-04-23 18:44:36 +0800

    add Logo in header

--- frontend/course-app/src/views/VueHeader.vue
+++ frontend/course-app/src/views/VueHeader.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="vue-header">
-    <nav-bar></nav-bar>
-  </div>
+  <el-row>
+    <el-col :span="4">
+      <div class="logo">Logo</div>
+    </el-col>
+    <el-col :span="20">
+      <nav-bar></nav-bar>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -15,6 +20,11 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+.logo {
+  line-height: 60px;
+  height: 60px;
+  text-align: center;
+  border-bottom: solid 1px #e6e6e6;
+}
 </style>
