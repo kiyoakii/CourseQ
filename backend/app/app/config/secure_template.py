2a89093d4b41214980ff4dd340fa4142b91ccf91
Jin <lijin110110@gmail.com>
2020-04-25 23:24:23 +0800

    add secure template

---
+++ backend/app/app/config/secure_template.py
@@ -0,0 +1,7 @@
+"""
+This is the template for secure.py, please created your own secure.py this way.
+"""
+SQLALCHEMY_DATABASE_URI = \
+    'mysql+cymysql://root:123456@localhost/name'
+
+SECRET_KEY = 'Lin Huancheng wudi'
