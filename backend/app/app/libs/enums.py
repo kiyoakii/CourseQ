023cabbe97b20800340fb660195688d311307a21
Jin <lijin110110@gmail.com>
2020-04-25 23:24:12 +0800

    add user enum

--- backend/app/app/libs/enums.py
+++ backend/app/app/libs/enums.py
@@ -1,1 +1,18 @@
 
+from enum import Enum
+
+class UserTypeEnum(Enum):
+    STUDENT = 1
+    TA = 2
+    TEACHER = 3
+    MANAGER = 4
+
+    @classmethod
+    def user_str(cls, status):
+        key_map = {
+            cls.STUDENT: '学生',
+            cls.TA: '助教',
+            cls.TEACHER: '老师',
+            cls.MANAGER: '管理员'
+        }
+        return key_map[status]
