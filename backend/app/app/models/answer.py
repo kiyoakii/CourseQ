fb9419c33454e3efe93ca913652cf1f6548f850e
Jin <lijin110110@gmail.com>
2020-04-25 23:22:18 +0800

    add answer, questions and discussions

---
+++ backend/app/app/models/answer.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+
+from app.models.base import Base
+
+class Answer(Base):
+    ansid = Column(Integer, primary_key=True)
+    qid = Column(Integer, ForeignKey('question.id'))
+    content = Column(String(500), nullable=False)
+    author = Column(String(10), ForeignKey('user.gid'))
+    stars = Column(Integer, default=0)
