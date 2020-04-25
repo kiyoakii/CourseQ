a8f1dae0dcba767beb57ff6478abd33946e2de24
HD <wangshunhong@mail.ustc.edu.cn>
2020-04-25 22:23:53 +0800

    finish Announcements.vue

--- frontend/course-app/src/components/CourseAnnounce.vue
+++ frontend/course-app/src/components/CourseAnnounce.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <!-- This is CourseAnnounce view. -->
-    <el-card shadow="always">
-      <div slot="header" class="announcement-info">
+    <el-card shadow="always" class="course-announce">
+      <div slot="header" class="announcements-header">
         <h1>Announcements</h1>
       </div>
-      <el-card v-if="announcements.length > 0" shadow="always" class="announcement-card">
-        <div slot="header" class="announcement-info">
-          <h3 class="announcement-title">{{ announcements[0].title }}</h3>
-          <div class="announcement-publish-info">
-            发布者：{{ announcements[0].publisher }}
-          </div>
-          <div class="announcement-publish-info">
-            发布时间：{{ announcements[0].time }}
-          </div>
-        </div>
-        <div class="announcement-content">
-          {{ announcements[0].content }}
-        </div>
-      </el-card>
-      <div v-else>暂时没有公告哦(*^__^*)</div>
-      <el-collapse v-if="announcements.length > 1">
-        <el-collapse-item name="1">
-          <template slot="title">
-            <h1 class="more-announcements-header">More</h1>
-          </template>
-          <el-scrollbar class="announcements-panel">
-            <el-card v-for="item in announcements.slice(1)" :key="item.title"
+      <el-scrollbar>
+          <el-card v-if="announcements.length > 0"
+            shadow="always" class="announcement-card">
+            <div slot="header" class="announcement-info">
+              <h3 class="announcement-title">{{ announcements[0].title }}</h3>
+              <div class="announcement-publish-info">
+                发布者：{{ announcements[0].publisher }}
+              </div>
+              <div class="announcement-publish-info">
+                发布时间：{{ announcements[0].time }}
+              </div>
+            </div>
+            <div class="announcement-content">
+              {{ announcements[0].content }}
+            </div>
+          </el-card>
+        <div v-else>暂时没有公告哦(*^__^*)</div>
+        <div v-if="announcements.length > 1"
+          v-bind:class="{'announcements-panel-active': this.isActive,
+          'announcements-panel-close': !this.isActive}">
+          <transition-group name="fade">
+            <el-card v-show="isActive" v-for="item in announcements.slice(1)" :key="item.title"
               shadow="always" class="announcement-card">
               <div slot="header" class="announcement-info">
                 <h3 class="announcement-title">{{ item.title }}</h3>
@@ -41,9 +41,13 @@
                 {{ item.content }}
               </div>
             </el-card>
-          </el-scrollbar>
-        </el-collapse-item>
-      </el-collapse>
+          </transition-group>
+        </div>
+      </el-scrollbar>
+      <el-button v-if="announcements.length > 1" @click="onClick" class="btn-more">
+        <i :class="isActive ? 'el-icon-caret-top' : 'el-icon-caret-bottom'"></i>
+        <span>{{ btnMassage }}</span>
+       </el-button>
     </el-card>
   </div>
 </template>
@@ -59,18 +63,85 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      isActive: false,
+      btnMassage: 'More',
+    };
+  },
+  methods: {
+    onClick() {
+      if (this.isActive === false) {
+        this.isActive = true;
+        this.btnMassage = 'Pack up';
+      } else {
+        this.isActive = false;
+        this.btnMassage = 'More';
+      }
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style>
 
-.announcement-card {
-  margin-top: 1em;
+.el-scrollbar .el-scrollbar__wrap {
+  max-height: 20em;
+  overflow-x:hidden;
 }
 
-.announcements-panel {
+.el-scrollbar__wrap .el-scrollbar__view {
+  height: 100%;
+}
+
+</style>>
+
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: all 1s;
+}
+.fade-enter {
+  opacity: 0;
+  transform: translate(-20em, -60em);
+}
+.fade-leave-to {
+  transform: translate(20em, 10em);
+}
+
+.fade-move {
+  transition: transform 1s;
+}
+
+.course-announce {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.btn-more {
   width: 100%;
-  height: 30em;
+  margin: 0;
+  background-color: #f9fafc;
+}
+
+.btn-more:hover {
+  color: #409eff;
+  background-color: #f9fafc;
+}
+
+.announcements-panel-active {
+  margin: 0;
+  width: 100%;
+  height: 20em;
+  transition: height .8s;
+}
+
+.announcements-panel-close {
+  margin: 0;
+  width: 100%;
+  height: 0em;
+  transition: height .8s;
 }
 
 .announcement-info {
@@ -83,6 +154,12 @@ export default {
 .more-announcements-header {
   font-weight:700;
   margin-left: 1em;
+}
+
+.announcement-card {
+  margin: 1em 0;
+  height: auto;
+  border: 0;
 }
 
 .announcement-title {
