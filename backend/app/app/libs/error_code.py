03a24f403f1618bbf46bad640e7ed9509c0673d7
Jin <lijin110110@gmail.com>
2020-04-25 23:22:41 +0800

    add error definition

---
+++ backend/app/app/libs/error_code.py
@@ -0,0 +1,54 @@
+from app.libs.errors import APIException
+
+
+class Success(APIException):
+    code = 201
+    msg = 'ok'
+    error_code = 0
+
+
+class DeleteSuccess(Success):
+    code = 202
+    error_code = 1
+
+
+class ServerError(APIException):
+    code = 500
+    msg = 'ServerError'
+    error_code = 999
+
+
+class ClientTypeError(APIException):
+    code = 400
+    msg = 'client is invalid'
+    error_code = 1006
+
+
+class ParameterException(APIException):
+    code = 400
+    msg = 'invalid parameter'
+    error_code = 1000
+
+
+class NotFound(APIException):
+    code = 404
+    msg = 'resource not found'
+    error_code = 1001
+
+
+class AuthFailed(APIException):
+    code = 401
+    error_code = 1005
+    msg = 'authorization failed'
+
+
+class Forbidden(APIException):
+    code = 403
+    error_code = 1004
+    msg = 'forbidden, not in scope'
+
+
+class Duplicate(APIException):
+    code = 400
+    error_code = 2001
+    msg = 'duplicate error'
