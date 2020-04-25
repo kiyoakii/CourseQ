e263cf6eccef47d2adbf5a59c783984ef9cb536e
Jin <lijin110110@gmail.com>
2020-04-25 23:23:25 +0800

    add email sending
    asynchronously sending email

---
+++ backend/app/app/libs/email.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from flask import current_app, render_template
+from flask_mail import Message,Mail
+
+mail = Mail()
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        try:
+            mail.send(msg)
+        except Exception as e:
+            pass
+
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.confid['MAIL_PREFIX'] + ' ' + subject,
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.html = render_template(template, **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
+
+
+
