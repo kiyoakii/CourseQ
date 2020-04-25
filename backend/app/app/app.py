3bcb3a368ed379dd3cb93e3a58927e9b33eb6de9
Jin <lijin110110@gmail.com>
2020-04-25 23:24:47 +0800

    update create_app

--- backend/app/app/app.py
+++ backend/app/app/app.py
@@ -1,6 +1,18 @@
-from . import create_app
-app = create_app()
+from flask import Flask as _Flask
+from flask.json import JSONEncoder as _JSONEncoder
+
+from app.libs.error_code import ServerError
+from datetime import date
 
 
-if __name__ == '__main__':
-    app.run()
+class JSONEncoder(_JSONEncoder):
+    def default(self, o):
+        if hasattr(o, 'keys') and hasattr(o, '__getitem__'):
+            return dict(o)
+        if isinstance(o, date):
+            return o.strftime('%Y-%m-%d')
+        raise ServerError()
+
+
+class Flask(_Flask):
+    json_encoder = JSONEncoder
