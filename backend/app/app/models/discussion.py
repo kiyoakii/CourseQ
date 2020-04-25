a193e1350ecb0d634a28c279cd30324a5c041371
Jin <lijin110110@gmail.com>
2020-04-25 23:22:18 +0800

    add answer, questions and discussions

---
+++ backend/app/app/models/discussion.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+
+from app.models.base import Base
+
+class Discussion(Base):
+    did = Column(Integer, primary_key=True)
+    qid = Column(Integer, ForeignKey('question.id'))
+    content = Column(String(500), nullable=False)
+    author = Column(String(10), ForeignKey('user.gid'))
+    stars = Column(Integer, default=0)
+
