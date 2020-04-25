b9119e347388f5a431b9f933de2be5bfc6e3f952
Jin <lijin110110@gmail.com>
2020-04-25 23:24:47 +0800

    update create_app

--- backend/app/app/__init__.py
+++ backend/app/app/__init__.py
@@ -1,12 +1,27 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from .app import Flask
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
 
+def register_blueprints(app):
+    from app.api.v1 import create_blueprint_v1
+    app.register_blueprint(create_blueprint_v1(), url_prefix='/v1')
+
+
+def register_plugin(app):
+    from app.models.base import db
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.setting')
+    app.config.from_object('app.config.secure')
+
+    register_blueprints(app)
+    register_plugin(app)
+
+    return app
 
 def create_app():
     app = Flask(__name__)
