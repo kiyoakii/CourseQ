db2c8d74ba6b03c3823b5f71b684650fd2c40bf5
Jin <lijin110110@gmail.com>
2020-04-25 23:21:47 +0800

    add course and enrollment

--- backend/app/app/models/course.py
+++ backend/app/app/models/course.py
@@ -1,1 +1,19 @@
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
+from app.models.base import Base
+from app.models.enroll import enroll_table
+
+
+class Course(Base):
+    id = Column(String(15), primary_key=True)
+    name_zh = Column(String(20), nullable=False)
+    name_en = Column(String(20), nullable=False)
+    # teachers, TAs, students
+    users = relationship('User', secondary=enroll_table,
+                         back_populates='courses')
+    intro = Column(String(200))
+    pre_Course = Column(String(50))
+    textbooks = Column(String(50))
+    semester = Column(String(50))
+    # To DO: semester validation
