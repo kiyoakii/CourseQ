b9355f4f7d5265c1b49f3f99c35e698564ea1392
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-26 21:37:58 +0800

    VueContent.vue: add el-aside

--- frontend/proans-app/src/views/VueContent.vue
+++ frontend/proans-app/src/views/VueContent.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    This is VueContent view.
-    <editor></editor>
+    <el-container>
+      <el-aside>
+        <problem-list></problem-list>
+      </el-aside>
+      <el-main>
+        <editor></editor>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 import Editor from '@/components/Editor.vue';
+import ProblemList from '@/components/ProblemList.vue';
 
 export default {
   name: 'VueContent',
   components: {
     Editor,
+    ProblemList,
   },
 };
 </script>
