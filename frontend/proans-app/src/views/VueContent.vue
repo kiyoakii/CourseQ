ff61cc72410698a40b402c91e58f71bdadb97f72
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-26 12:43:03 +0800

    proans-app: add mavon editor

--- frontend/proans-app/src/views/VueContent.vue
+++ frontend/proans-app/src/views/VueContent.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     This is VueContent view.
+    <editor></editor>
   </div>
 </template>
 
 <script>
+import Editor from '@/components/Editor.vue';
+
 export default {
   name: 'VueContent',
+  components: {
+    Editor,
+  },
 };
 </script>
 
