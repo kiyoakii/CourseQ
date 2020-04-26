3f0d7b1edf69507a1193e56b60e026f3fcbe919a
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-26 12:43:03 +0800

    proans-app: add mavon editor

--- frontend/proans-app/src/main.js
+++ frontend/proans-app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element';
+import './plugins/vue-mavon-editor';
 
 Vue.config.productionTip = false;
 
