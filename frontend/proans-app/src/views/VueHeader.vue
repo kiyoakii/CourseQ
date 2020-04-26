45e0ae97846cdeccb277c8919c772dcd40eb63a6
zsStrike <2439759713@qq.com>
2020-04-26 15:14:03 +0800

    proans: add profile icon & my-pack btn

--- frontend/proans-app/src/views/VueHeader.vue
+++ frontend/proans-app/src/views/VueHeader.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="header">
-    <problem-categories></problem-categories>
-  </div>
+  <el-row class="header">
+    <el-col :span="20">
+      <problem-categories></problem-categories>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="space-around">
+        <el-button>我的关注</el-button>
+        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -16,5 +24,7 @@ export default {
 </script>
 
 <style scoped>
-
+.header {
+  margin: 10px 0;
+}
 </style>
