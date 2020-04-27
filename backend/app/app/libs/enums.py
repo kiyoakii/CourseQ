9d567ffa73f0230421a1ce766493dc27bf9827e2
Jin <lijin110110@gmail.com>
2020-04-27 21:05:10 +0800

    delete TA

--- backend/app/app/libs/enums.py
+++ backend/app/app/libs/enums.py
@@ -3,15 +3,13 @@ from enum import Enum
 
 class UserTypeEnum(Enum):
     STUDENT = 1
-    TA = 2
-    TEACHER = 3
-    MANAGER = 4
+    TEACHER = 2
+    MANAGER = 3
 
     @classmethod
     def user_str(cls, status):
         key_map = {
             cls.STUDENT: '学生',
-            cls.TA: '助教',
             cls.TEACHER: '老师',
             cls.MANAGER: '管理员'
         }
