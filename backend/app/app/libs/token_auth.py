b582fdbc7d76d7abcfb939401042b49a470a7992
Jin <lijin110110@gmail.com>
2020-04-27 21:01:02 +0800

    update token_auth

--- backend/app/app/libs/token_auth.py
+++ backend/app/app/libs/token_auth.py
@@ -1,1 +1,37 @@
+from collections import namedtuple
 
+from flask import current_app, g, request
+from flask_httpauth import HTTPBasicAuth
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+
+from app.libs.error_code import AuthFailed, Forbidden
+from app.libs.scope import is_in_scope
+
+auth = HTTPBasicAuth()
+User = namedtuple('User', ['gid', 'scope'])
+
+
+@auth.verify_password
+def verify_password(token, password):
+    user = verify_auth_token(token)
+    if not user:
+        return False
+    else:
+        g.user = user
+        return True
+
+
+def verify_auth_token(token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except BadSignature:
+        raise AuthFailed(msg='token is invalid', error_code=1002)
+    except SignatureExpired:
+        raise AuthFailed(msg='token is expired', error_code=1003)
+    gid = data['gid']
+    scope = data['scope']
+    allow = is_in_scope(scope, request.endpoint)
+    if not allow:
+        raise Forbidden()
+    return User(gid, scope)
