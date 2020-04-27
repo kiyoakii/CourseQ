331808d45855475d2077863be74084543ed8790c
zfming <zfmfengming@mail.ustc.edu.cn>
2020-04-28 00:37:11 +0800

    Update CourseSchedule.vue, Home.vue(for write schedule data)

--- frontend/course-app/src/components/CourseSchedule.vue
+++ frontend/course-app/src/components/CourseSchedule.vue
@@ -1,15 +1,70 @@
 <template>
   <div>
-    This is CourseSchedule component.
+    <!-- This is CourseSchedule view. -->
+    <el-card shadow="always">
+      <div slot="header">
+        <h1>Schedule</h1>
+      </div>
+      <el-card>
+        <el-table :data="schedule" highlight-current-row size="medium">
+          <el-table-column prop="week" label="Week" width="100" align="center"
+            style="font-weight:bold"></el-table-column>
+          <el-table-column label="Course 1st" width="260" align="left"
+            :show-overflow-tooltip="true">
+            <template slot-scope="scope">
+              <div>
+                <ul>
+                  <li v-for="(value, index) in scope.row.courseOne" :key="index">
+                    {{`${index}:${value}`}}
+                  </li>
+                </ul>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="Course 2nd" width="260" align="left"
+            :show-overflow-tooltip="true">
+            <template slot-scope="scope">
+              <div>
+                <ul>
+                  <li v-for="(value, index) in scope.row.courseTwo" :key="index">
+                    {{`${index}:${value}`}}
+                  </li>
+                </ul>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column prop="assignments" label="Assignments" align="left"></el-table-column>
+        </el-table>
+      </el-card>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CourseSchedule',
+  props: {
+    schedule: {
+      type: Array,
+      default() {
+        return [
+          {
+            week: 'Week1',
+            courseOne: null,
+            courseTwo: {
+              lecture: 'Lec1',
+              describe: 'Introduction',
+              reference: 'xxx',
+            },
+            assignments: 'Homework 1',
+          },
+        ];
+      },
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
 
 </style>
