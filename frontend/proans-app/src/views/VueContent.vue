f3a3ae56dc78a71ab1d661c50b9986f3a2fe31cd
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-27 21:27:19 +0800

    finish VueContent.vue

--- frontend/proans-app/src/views/VueContent.vue
+++ frontend/proans-app/src/views/VueContent.vue
@@ -2,9 +2,14 @@
   <div>
     <el-container>
       <el-aside>
+        <search></search>
         <problem-list></problem-list>
       </el-aside>
       <el-main>
+        <problem-card></problem-card>
+        <answer-card></answer-card>
+        <discussion></discussion>
+        <time-line></time-line>
         <editor></editor>
       </el-main>
     </el-container>
@@ -14,12 +19,22 @@
 <script>
 import Editor from '@/components/Editor.vue';
 import ProblemList from '@/components/ProblemList.vue';
+import Search from '@/components/Search.vue';
+import TimeLine from '@/components/TimeLine.vue';
+import ProblemCard from '@/components/ProblemCard.vue';
+import AnswerCard from '@/components/AnswerCard.vue';
+import Discussion from '@/components/Discussion.vue';
 
 export default {
   name: 'VueContent',
   components: {
     Editor,
     ProblemList,
+    Search,
+    TimeLine,
+    ProblemCard,
+    AnswerCard,
+    Discussion,
   },
 };
 </script>
