bcb79b043500b3b8f3611371b78869c9d2959c5c
Jin <lijin110110@gmail.com>
2020-04-27 21:04:16 +0800

    update models

--- backend/app/app/models/enroll.py
+++ backend/app/app/models/enroll.py
@@ -1,5 +1,9 @@
 from sqlalchemy import Column, String, ForeignKey, Table
 
-enroll_table = Table('association',
-                     Column('user_id', String(10), ForeignKey('User.id')),
-                     Column('course_id', String(15), ForeignKey('Course.id')))
+enroll_table = Table('enroll',
+                     Column('user_id', String(10), ForeignKey('user.gid')),
+                     Column('course_id', String(15), ForeignKey('course.cid')))
+
+# ta_table = Table('ta',
+#                  Column('user_id', String(10), ForeignKey('user.GID')),
+#                  Column('course_id', String(15), ForeignKey('course.cid')))
--- backend/app/app/models/question.py
+++ backend/app/app/models/question.py
@@ -5,7 +5,7 @@ from app.models.base import Base
 
 class Question(Base):
     qid = Column(Integer, primary_key=True)
-    author_id = Column(String(10), ForeignKey('user.GID'))
+    author_id = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
     answer = Column(Integer, ForeignKey('answer.id'), default=None)
     discussions = relationship('Discussion', backref='question')
