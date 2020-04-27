b87b4f3206ad12e249e26a399a319ea2fd39934c
Jin <lijin110110@gmail.com>
2020-04-27 21:07:41 +0800

    replace auth with user
    auth was originally written by lhc

---
+++ backend/app/app/api/v1/user.py
@@ -0,0 +1,43 @@
+from flask import request, json, jsonify, g
+
+from app.libs.error_code import Success, DeleteSuccess
+from app.libs.redprint import Redprint
+from app.models.base import db
+from app.models.user import User
+from app.validators.forms import UserForm
+from app.libs.token_auth import auth
+api = Redprint('User')
+
+
+@api.route('/<string:gid>', methods=['GET'])
+@auth.login_required
+def super_get_user(gid):
+    user = User.query.filter_by(gid=gid).first_or_404()
+    return jsonify(user)
+
+
+@api.route('', methods=['GET'])
+@auth.login_required
+def get_user():
+    gid = g.user.gid
+    user = User.query.filter_by(gid=gid).first_or_404()
+    return jsonify(user)
+
+
+@api.route('', methods=['DELETE'])
+@auth.login_required
+def delete_user():
+    gid = g.user.gid
+    with db.auto_commit():
+        user = User.query.filter_by(gid=gid).first_or_404()
+        user.delete()
+    return DeleteSuccess()
+
+
+@api.route('/register', methods=['POST'])
+def register():
+    form = UserForm().validate_for_api()
+    user = User()
+    user.register(form.nickname.data, form.email.data,
+                  g.user.gid, g.user.uid)
+    return Success()
