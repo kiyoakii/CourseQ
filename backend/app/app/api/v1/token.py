de7b4809753c6e07c4a05752e9244c8a1069d5df
Jin <lijin110110@gmail.com>
2020-04-27 21:01:02 +0800

    update token_auth

--- backend/app/app/api/v1/token.py
+++ backend/app/app/api/v1/token.py
@@ -1,1 +1,42 @@
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from xml.etree import ElementTree
+
+from flask import current_app, jsonify, url_for, request
+
+from app.libs.error_code import Redirect
+from app.libs.redprint import Redprint
+from app.models.user import User
+from app.validators.forms import UserForm
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+api = Redprint('token')
+
+
+@api.route('', methods=['GET'])
+def get_token():
+    ticket = request.args.get("ticket")
+    service = ''
+    identity = User.verify(ticket, service)
+
+
+    expiration = current_app.config['TOKEN_EXPIRATION']
+    token = generate_auto_token(identity['gid'],
+                                identity['scope'],
+                                expiration)
+    t = {
+        'token': token.decode('ascii')
+    }
+    return jsonify(t), 201
+
+
+def generate_auto_token(gid, scope=None, expiration=7200):
+    """generate token"""
+    s = Serializer(current_app.config['SECRET_KEY'],
+                   expires_in=expiration)
+    return s.dumps({
+        'GID': gid,
+        'scope': scope
+    })
 
