67385b107aabcec755ca8491caa1223c49997c71
Jin <lijin110110@gmail.com>
2020-04-28 20:54:37 +0800

    fix Userform

--- backend/course/app/validators/base.py
+++ backend/course/app/validators/base.py
@@ -8,7 +8,7 @@ class BaseForm(Form):
     def __init__(self):
         data = request.get_json(silent=True)
         args = request.args.to_dict()
-        super(BaseForm, self).__init__(data=data, **args)
+        super().__init__(data=data, **args)
 
     def validate_for_api(self):
         valid = super(BaseForm, self).validate()
