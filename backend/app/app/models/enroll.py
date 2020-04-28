e1a0f89d5aaeead9dd8af9269990d55ec9a8e503
biuren <biuren@mail.ustc.edu.cn>
2020-04-28 12:32:59 +0800

    backend: specify metadata for enroll table

--- backend/app/app/models/enroll.py
+++ backend/app/app/models/enroll.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, ForeignKey, Table
+from .base import Base
 
 enroll_table = Table('enroll',
+                     Base.metadata,
                      Column('user_id', String(10), ForeignKey('user.gid')),
                      Column('course_id', String(15), ForeignKey('course.cid')))
 
