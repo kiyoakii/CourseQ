5a52fed48dab2820f8802eef732d399b4ddae04b
HD <wangshunhong@mail.ustc.edu.cn>
2020-04-28 21:30:33 +0800

    finish ProblemCatgorties.vue & TimeLine.vue

--- frontend/proans-app/src/views/TimeLine.vue
+++ frontend/proans-app/src/views/TimeLine.vue
@@ -1,15 +1,183 @@
 <template>
-  <div>
-    This is the TimeLine component.
-  </div>
+  <el-card class="box-card">
+    <!-- This is the TimeLine component. -->
+    <div slot="header" class="clearfix">
+      <span>版本时间轴</span>
+    </div>
+    <el-scrollbar class= "timeline">
+      <ul class="timeline-list">
+        <li class="timeline-item" v-for="(item, i) in timelineList" :key="i"
+        @click="chooseVersion(item.versionId)">
+          <router-link :to="'./' + item.versionId">
+            <div v-if="i !== 0"
+            class="timeline-item-line timeline-item-line-left"></div>
+            <div v-if="i != timelineList.length - 1"
+            class="timeline-item-line timeline-item-line-right"></div>
+            <el-tooltip class="item" effect="dark"
+            :content="'用户： ' + item.userId + ' 进行的修改'" placement="top">
+              <div class="timeline-item-node"
+              :class="activeVersion === item.versionId ? 'timeline-item-node-active' : ''"></div>
+            </el-tooltip>
+            <div class="timeline-item-content">
+              <div class="timeline-item-desc">
+                {{ item.desc }}
+              </div>
+              <div class="timeline-item-time">{{ item.time }}</div>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </el-scrollbar>
+  </el-card>
 </template>
 
 <script>
 export default {
   name: 'TimeLine',
+  props: {
+    timelineList: {
+      type: Array,
+      default() {
+        return [
+          {
+            versionId: '0',
+            time: '2020-04-29',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '1',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '2',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '3',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '4',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '5',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+          {
+            versionId: '6',
+            time: '2020-04-28',
+            userId: '1234567',
+            desc: '使用动态规划可以将复杂度降为O(n)',
+          },
+        ];
+      },
+    },
+  },
+  data() {
+    return {
+      activeVersion: '0',
+    };
+  },
+  methods: {
+    chooseVersion(id) {
+      this.activeVersion = id;
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+
+a {
+  text-decoration: none;
+}
+
+.timeline-container {
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+}
+.timeline {
+  display: flex;
+  position: relative;
+  justify-content: center;
+  width: 100%;
+}
+
+.timeline-list {
+  display: flex;
+}
+
+.timeline-item {
+  position: relative;
+  display: flex;
+  width: 200px;
+  flex-shrink: 0;
+  justify-items: baseline;
+}
+.timeline-item-node {
+  position: absolute;
+  background-color: #e4e7ed;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 50%;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+.timeline-item-node-active {
+  background-color: #409eff;
+}
+
+.timeline-item-line {
+  position: absolute;
+  width: 50%;
+  top: 10px;
+  border-top: 2px solid #e4e7ed;
+}
+
+.timeline-item-line-left {
+  left: 0;
+}
+
+.timeline-item-line-right {
+  right: 0;
+}
+
+.timeline-prev, .timeline-next {
+}
+
+.timeline-item-content {
+  width: 100%;
+  padding: 28px 10px 0 10px;
+  color: black;
+}
+
+.timeline-item-desc {
+  text-align: center;
+  font-size: 14px;
+  font-weight: 300;
+}
+
+.timeline-item-time {
+  text-align: center;
+  font-size: 12px;
+  margin-top:10px;
+  font-weight: 200;
+}
 
 </style>
