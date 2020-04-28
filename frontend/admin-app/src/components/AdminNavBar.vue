c5a6d05dbb8e9178983b0aff54e6817920a3937b
zsStrike <2439758713@qq.com>
2020-04-28 22:08:35 +0800

    admin: finish AdminNavBar

---
+++ frontend/admin-app/src/components/AdminNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-menu default-active="/admin/course-manage" class="el-menu-demo"
+    mode="horizontal" router>
+      <el-menu-item index="/admin/course-manage">课程管理</el-menu-item>
+      <el-menu-item index="/admin/teacher-manage">教师管理</el-menu-item>
+      <el-menu-item index="/admin/student-manage">学生管理</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminNavBar',
+};
+</script>
+
+<style scoped>
+</style>
