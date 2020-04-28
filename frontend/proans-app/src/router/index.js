ecbe62344fdff14deae961dd97f5a3290f7528cb
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-28 11:54:31 +0800

    change router (Editor)

--- frontend/proans-app/src/router/index.js
+++ frontend/proans-app/src/router/index.js
@@ -17,12 +17,6 @@ const routes = [
     name: 'TimeLine',
     component: () => import('../views/TimeLine.vue'),
   },
-  {
-    // c_id: category_id, p_id: problem_id
-    path: '/:c_id/:p_id/editor',
-    name: 'Editor',
-    component: () => import('../views/Editor.vue'),
-  },
 ];
 
 const router = new VueRouter({
