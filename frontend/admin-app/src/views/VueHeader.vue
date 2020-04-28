104d364511883534e6a53cb254730cd48d311cc2
zsStrike <2439758713@qq.com>
2020-04-28 20:25:48 +0800

    admin: finish VueHeader

--- frontend/admin-app/src/views/VueHeader.vue
+++ frontend/admin-app/src/views/VueHeader.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-    This is VueHeader component.
-  </div>
+  <el-row class="header" type="flex" justify="space-between">
+    <el-col :span="4">
+      <div class="logo">
+        <!-- In Vue tmplate, import a image is tricky， we use webpack require syntax here -->
+        <div class="logo-img"
+        :style="{ backgroundImage: 'url(' + require('@/assets/logo.png') + ')' }">
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="2">
+      <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+
 export default {
   name: 'VueHeader',
+  components: {
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.header {
+  margin: 10px 0;
+}
 
+.logo {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo-img {
+  height: 40px;
+  width: 80px;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
 </style>
