afd334bdde28cf814940bca4fdd52efe9f5068e5
zsStrike <2439758713@qq.com>
2020-04-28 22:09:09 +0800

    admin: finish admin route

--- frontend/admin-app/src/router/index.js
+++ frontend/admin-app/src/router/index.js
@@ -1,22 +1,47 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home,
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/teacher',
+    name: 'Teacher',
+    components: {
+      header: () => import('../components/TeacherNavBar.vue'),
+      content: () => import('../components/TeacherCourseList.vue'),
+    },
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    redirect: '/admin/course-manage',
+  },
+  {
+    path: '/admin/course-manage',
+    name: 'CourseManage',
+    components: {
+      header: () => import('../components/AdminNavBar.vue'),
+      content: () => import('../views/AdminCourseManage.vue'),
+    },
+  },
+  {
+    path: '/admin/teacher-manage',
+    name: 'TeacherManage',
+    components: {
+      header: () => import('../components/AdminNavBar.vue'),
+      content: () => import('../views/AdminTeacherManage.vue'),
+    },
+  },
+  {
+    path: '/admin/student-manage',
+    name: 'StudentManage',
+    components: {
+      header: () => import('../components/AdminNavBar.vue'),
+      content: () => import('../views/AdminStudentManage.vue'),
+    },
+  },
 ];
 
 const router = new VueRouter({
