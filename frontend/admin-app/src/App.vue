03e36dd4046a6421dcb768e5d92c5224953b76f7
zsStrike <2439758713@qq.com>
2020-04-28 20:25:19 +0800

    admin: finish App

--- frontend/admin-app/src/App.vue
+++ frontend/admin-app/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <div class="header">
-      <vue-header></vue-header>
-    </div>
-    <div class="content">
-      <vue-content></vue-content>
-    </div>
+    <el-container>
+      <el-header>
+        <vue-header></vue-header>
+      </el-header>
+      <el-main>
+        <el-row type="flex" justify="center">
+          <el-col :xs="24" :sm="24" :md="22" :lg="22" :xl="22" >
+            <vue-content></vue-content>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -23,11 +29,23 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC,
+               Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-size: 16px;
+}
+
+.el-header {
+  padding: 0 !important;
+  border-bottom: 1px solid #eeeeee;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
 }
 </style>
