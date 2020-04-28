54f02cc88b9233944abe00be5255220b379a4865
Jin <lijin110110@gmail.com>
2020-04-28 15:50:03 +0800

    update to "flask run"
    app.run is deprecated

--- backend/course/app/__init__.py
+++ backend/course/app/__init__.py
@@ -22,20 +22,3 @@ def create_app():
     register_plugin(app)
 
     return app
-
-# def create_app():
-#     app = Flask(__name__)
-#     db.init_app(app)
-#     login_manager.init_app(app)
-#     app.config['SECRET_KEY'] = 'your secret key'
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-#
-#     from .models import User
-#
-#     @login_manager.user_loader
-#     def load_user(user_id):
-#         return User.query.get(int(user_id))
-#
-#     from .auth import auth as auth_blueprint
-#     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-#     return app
