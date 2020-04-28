2e94a6da5f9026957c91e3545af18c00ae45e5bb
zsStrike <2439758713@qq.com>
2020-04-28 22:34:32 +0800

    admin: finish TeacherNavBar

---
+++ frontend/admin-app/src/components/TeacherNavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-breadcrumb class="navbar" separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{'path': '/teacher/course-list'}">课程列表</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{'path': '/teacher/semester-list'}">2019年春</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{'path': '/teacher/course'}">计算方法</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeacherNavBar',
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 60px;
+  line-height: 60px;
+  font-size: 14px;
+}
+</style>
