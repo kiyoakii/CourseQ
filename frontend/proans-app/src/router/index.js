e351224853880e2dbe05362d4a0609405f9a1ded
IcePear-Jzx <icepear.jzx@gmail.com>
2020-04-29 22:39:09 +0800

    change router & layout

--- frontend/proans-app/src/router/index.js
+++ frontend/proans-app/src/router/index.js
@@ -2,22 +2,30 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import SignIn from '@/views/SignIn.vue';
 import Profile from '@/views/Profile.vue';
+import VueContent from '@/views/VueContent.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    // c_id: category_id, p_id: problem_id
-    path: '/:c_id/:p_id/answer',
-    name: 'Answer',
-    component: () => import('../views/AnswerView.vue'),
-    alias: '/:c_id/:p_id',
-  },
-  {
-    // c_id: category_id, p_id: problem_id
-    path: '/:c_id/:p_id/timeline',
-    name: 'TimeLine',
-    component: () => import('../views/TimeLine.vue'),
+    path: '/',
+    name: 'Content',
+    component: VueContent,
+    children: [
+      {
+        // c_id: category_id, p_id: problem_id
+        path: '/:c_id/:p_id/answer',
+        name: 'Answer',
+        component: () => import('../views/AnswerView.vue'),
+        alias: '/:c_id/:p_id',
+      },
+      {
+        // c_id: category_id, p_id: problem_id
+        path: '/:c_id/:p_id/timeline',
+        name: 'TimeLine',
+        component: () => import('../views/TimeLine.vue'),
+      },
+    ],
   },
   {
     path: '/signin',
