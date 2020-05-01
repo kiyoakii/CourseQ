3e6f76a010753c69674af083dcd75691ba6d7ca3
biuren <biuren@mail.ustc.edu.cn>
2020-05-02 03:59:57 +0800

    backend: Add TA type for enroll table

--- backend/course/app/libs/enums.py
+++ backend/course/app/libs/enums.py
@@ -1,17 +1,18 @@
-
 from enum import Enum
+
 
 class UserTypeEnum(Enum):
     STUDENT = 1
     TEACHER = 2
     MANAGER = 3
+    TA = 4
 
     @classmethod
     def user_str(cls, status):
         key_map = {
             cls.STUDENT: '学生',
             cls.TEACHER: '老师',
-            cls.MANAGER: '管理员'
+            cls.MANAGER: '管理员',
+            cls.TA: '助教',
         }
         return key_map[status]
-
