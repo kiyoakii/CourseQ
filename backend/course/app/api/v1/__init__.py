bc5211b3aa284459a7aa174e239b2eb17f08a03c
biuren <biuren@mail.ustc.edu.cn>
2020-05-02 03:59:39 +0800

    backend: New course view

--- backend/course/app/api/v1/__init__.py
+++ backend/course/app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
-from app.api.v1 import user, token
+from app.api.v1 import user, token, course
 
 
 def create_blueprint_v1():
     bp_v1 = Blueprint('v1', __name__)
     user.api.register(bp_v1, url_prefix='/user')
     token.api.register(bp_v1, url_prefix='/token')
+    course.api.register(bp_v1, url_prefix='/courses')
     return bp_v1
