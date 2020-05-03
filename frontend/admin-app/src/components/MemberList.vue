7af0b7bc1c1e3c4953c47688d57c6ffdbbb618e1
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-03 16:50:34 +0800

    finish MemberList

---
+++ frontend/admin-app/src/components/MemberList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <el-table :data="memberData" border id="member-table">
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="id" label="学号"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+      <el-table-column prop="phone" label="电话"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberList',
+  props: {
+    memberData: {
+      type: Array,
+      default() {
+        return [
+          {
+            name: 'name1',
+            id: 'id1',
+            email: 'email1',
+            phone: 'phone1',
+          },
+          {
+            name: 'name2',
+            id: 'id2',
+            email: 'email2',
+            phone: 'phone2',
+          },
+          {
+            name: 'name3',
+            id: 'id3',
+            email: 'email3',
+            phone: 'phone3',
+          },
+        ];
+      },
+    },
+  },
+  methods: {
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#member-table {
+  width: 800px;
+}
+</style>
