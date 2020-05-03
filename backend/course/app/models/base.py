50b8ccad926be7a946f9a10d82600f6ca826788f
Jin <lijin110110@gmail.com>
2020-05-03 14:03:20 +0800

    remove redundant class

--- backend/course/app/models/base.py
+++ backend/course/app/models/base.py
@@ -77,33 +77,3 @@ class Base(db.Model):
         for key in keys:
             self.fields.append(key)
         return self
-
-
-class MixinJSONSerializer:
-    @orm.reconstructor
-    def init_on_load(self):
-        self._fields = []
-        self._exclude = []
-
-        self._set_fields()
-        self.__prune_fields()
-
-    def _set_fields(self):
-        pass
-
-    def __prune_fields(self):
-        columns = inspect(self.__class__).columns
-        if not self._fields:
-            all_columns = set(columns.keys())
-            self._fields = list(all_columns - set(self._exclude))
-
-    def hide(self, *args):
-        for key in args:
-            self._fields.remove(key)
-        return self
-
-    def keys(self):
-        return self._fields
-
-    def __getitem__(self, key):
-        return getattr(self, key)
