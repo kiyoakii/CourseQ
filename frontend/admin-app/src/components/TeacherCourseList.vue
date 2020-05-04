8a54aa57232ce9811f83ae6a6e5b01c114b0c5c6
nnnyt <793313994@qq.com>
2020-05-03 22:30:50 -0700

    finish TeacherCourseList.vue

--- frontend/admin-app/src/components/TeacherCourseList.vue
+++ frontend/admin-app/src/components/TeacherCourseList.vue
@@ -1,15 +1,58 @@
 <template>
   <div>
-    This is TeacherCourseList component.
+    <!-- This is TeacherCourseList component. -->
+    <el-row :gutter="40">
+      <el-col :span="8" v-for="(item, i) in courses" :key="i">
+        <router-link :to="'/teacher/semester-list'">
+          <el-button type="primary" plain>
+              {{item.name}}
+          </el-button>
+        </router-link>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TeacherCourseList',
+  props: {
+    courses: {
+      type: Array,
+      default() {
+        return [
+          {
+            name: '计算方法',
+            link: 'course',
+          },
+          {
+            name: '计算方法',
+            link: '2',
+          },
+          {
+            name: '计算方法',
+            link: '3',
+          },
+          {
+            name: '计算方法',
+            link: '4',
+          },
+        ];
+      },
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.el-row {
+  width: 1000px;
 
+}
+.el-button {
+  height: 120px;
+  width: 100%;
+  font-size: 20px;
+  margin-bottom: 30px;
+}
 </style>
