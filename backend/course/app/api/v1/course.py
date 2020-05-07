81893e36689f146eda32019e5142c1061ee7772c
biuren <biuren@mail.ustc.edu.cn>
2020-05-07 15:58:42 +0800

    backend: separate files and courses

--- backend/course/app/api/v1/course.py
+++ backend/course/app/api/v1/course.py
@@ -1,6 +1,7 @@
 from flask import jsonify, g, request, send_file
-from app.models.course import Course, CourseResource
+from app.models.course import Course
 from app.models.user import User
+from app.models.resource import CourseResource
 from app.models.enroll import Enroll
 from app.libs.error_code import Success, DeleteSuccess, Forbidden, ParameterException
 from app.libs.redprint import Redprint
@@ -14,6 +15,10 @@ api = Redprint('course')
 
 
 # TODO gid check and scope
+@api.route('', methods=['GET'])
+def get_course_list():
+    return jsonify(courses=Course.query.all())
+
 
 @api.route('/create', methods=['POST'])
 def create_course():
@@ -55,27 +60,6 @@ def delete_course(cid):
     return DeleteSuccess()
 
 
-@api.route('/<int:cid>/file', methods=['POST'])
-def upload(cid):
-    try:
-        file = request.files['file']
-    except ValueError:
-        return ParameterException
-    with db.auto_commit():
-        filename = secure_filename(file.filename)
-        data = file.read()
-        resource = CourseResource()
-        resource.name = filename
-        resource.data = data
-        db.session.add(resource)
-    return Success()
-
-
-@api.route('/<int:cid>/file', methods=['GET'])
-def get_file_list(cid):
-    pass
-
-
-@api.route('<int:cid>/file/<int:fid>', methods=['GET'])
-def download(cid, fid):
+@api.route('/<int:cid>/files', methods=['GET'])
+def get_files_list(cid):
     pass
