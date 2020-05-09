bc34045e06794df7a583fc03bb48ccfa7fb51604
zsStrike <2439758713@qq.com>
2020-05-09 11:54:52 +0800

    admin: finish CourseAnnounce

--- frontend/admin-app/src/views/CourseAnnounce.vue
+++ frontend/admin-app/src/views/CourseAnnounce.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    This is CourseAnnounce component.
+  <div class="course-announce">
+    <el-row class="announce" v-for="anno in announceList" :key="anno.id">
+      <el-card class="card-box">
+        <div slot="header" class="clearfix card-header">
+          <span>{{ anno.title }}</span>
+          <el-button type="primary"
+          icon="el-icon-edit" size="small">
+          </el-button>
+        </div>
+        <div>
+          {{ anno.content }}
+        </div>
+      </el-card>
+    </el-row>
+    <el-row>
+      <el-button type="primary">新建公告</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CourseIntro',
+  name: 'CourseAnnounce',
+  data() {
+    return {
+      announceList: [
+        {
+          title: 'Announcement1',
+          content: 'This is Announcement1, This is Announcement1, This is Announcement1, This is Announcement1',
+          id: 1,
+        },
+        {
+          title: 'Announcement2',
+          content: 'This is Announcement1, This is Announcement1, This is Announcement1, This is Announcement1',
+          id: 2,
+        },
+        {
+          title: 'Announcement2',
+          content: 'This is Announcement1, This is Announcement1, This is Announcement1, This is Announcement1',
+          id: 3,
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
-
+.announce {
+  margin-bottom: 15px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
