5154bc3ed90974db4bb7c75527cd68ad82e9f4f3
zsStrike <2439758713@qq.com>
2020-05-09 12:16:51 +0800

    admin: update TeacherNavBar, making it changable

--- frontend/admin-app/src/components/TeacherNavBar.vue
+++ frontend/admin-app/src/components/TeacherNavBar.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <el-breadcrumb class="navbar" separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{'path': '/teacher/course-list'}">课程列表</el-breadcrumb-item>
-      <el-breadcrumb-item :to="{'path': '/teacher/semester-list'}">2019年春</el-breadcrumb-item>
-      <el-breadcrumb-item :to="{'path': '/teacher/course'}">计算方法</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{'path': '/teacher/course-list'}"
+      v-if="isCourseList || isSemesterList || isCourse">课程列表</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{'path': '/teacher/semester-list'}"
+      v-if="isSemesterList || isCourse">学期列表</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{'path': '/teacher/course'}"
+      v-if="isCourse">计算方法</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
@@ -11,6 +14,24 @@
 <script>
 export default {
   name: 'TeacherNavBar',
+  data() {
+    return {
+      // isCourseList: true,
+      // isSemesterList: false,
+      // isCourse: false,
+    };
+  },
+  computed: {
+    isCourseList() {
+      return this.$route.path === '/teacher/course-list';
+    },
+    isSemesterList() {
+      return this.$route.path === '/teacher/semester-list';
+    },
+    isCourse() {
+      return this.$route.path.indexOf('/teacher/course/') !== -1;
+    },
+  },
 };
 </script>
 
