62948c2d25513d836b53c6c2ec55f8be1f566a6f
zfming <zfmfengming@mail.ustc.edu.cn>
2020-05-10 13:44:06 +0800

    Update CourseIntro.vue in admin-app/views

--- frontend/admin-app/src/views/CourseIntro.vue
+++ frontend/admin-app/src/views/CourseIntro.vue
@@ -1,15 +1,81 @@
 <template>
-  <div>
-    This is CourseIntro component.
+  <div class="intro">
+    <el-card class="box-card" shadow="hover">
+      <div slot="header" class="clearfix">
+        <span class="title">{{courseName}}</span>
+        <div class="footer">
+          {{courseTerm}}
+        </div>
+      </div>
+      <div class="text item">
+        {{ courseIntroduction }}
+      </div>
+    </el-card>
+    <el-card class="message-card">
+      <el-row>
+        <div class="text item">
+          <span class="subtitle">教师信息：</span>
+          <span>{{courseTutor}}</span>
+        </div>
+      </el-row>
+      <el-row>
+        <div class="text item">
+          <span class="subtitle">上课时间：</span>
+          <span>{{courseTime}}</span>
+        </div>
+      </el-row>
+      <el-row>
+        <div class="text item">
+          <span class="subtitle">参考教材：</span>
+          <span>{{courseReference}}</span>
+        </div>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CourseIntro',
+  data() {
+    return {
+      courseName: '人工智能导论',
+      courseTerm: '2020 Spring',
+      courseIntroduction: `人工智能是一个庞杂的学科体系，从概念上讲，
+                          一切为复制生物智能而做出的努力都可纳入其中。
+                          如何能够系统、全面、简洁地描述人工智能的全貌而不显得凌乱，
+                          绝非易事。主要教授如何选购扫地机器人`,
+      courseTutor: 'Jianmin Ji',
+      courseTime: '15:55-17:30 Monday, 9:45-11:20 Wednesday',
+      courseReference: '周志华 《西瓜书》',
+    };
+  },
 };
 </script>
 
 <style>
-
+.tags {
+  margin-left: 10px;
+}
+.el-tag {
+  font-size: 10px;
+}
+.title {
+  font-size: 30px;
+}
+.subtitle {
+  font-size: 22px;
+}
+.footer {
+  padding-top: 20px;
+  display: flex;
+  justify-content: flex-end;
+}
+.intro {
+  margin:0 auto;
+  width:700px;
+}
+.message-card {
+  margin-top: 50px;
+}
 </style>
