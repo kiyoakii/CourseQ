16ff6b90f9f4150b1758f48d234ef24d42b70b50
HD <wangshunhong@mail.ustc.edu.cn>
2020-05-11 23:02:27 +0800

    add AdminMemberList.vue and AdminMemberListSearch.vue

---
+++ frontend/admin-app/src/components/AdminMemberListSearch.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-form :inline="true" :model="searchForm">
+      <el-form-item class="form-item">
+        <el-input v-model="searchForm.name" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item class="form-item">
+        <el-input v-model="searchForm.id"
+        :placeholder="identity === 'teacher' ? '工号' : '学号'"></el-input>
+      </el-form-item>
+      <el-form-item class="form-item">
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminMemberListSearch',
+  props: {
+    identity: {
+      type: String,
+      default() {
+        return 'student';
+      },
+    },
+  },
+  data() {
+    return {
+      searchForm: {
+        name: '',
+        id: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-item {
+  margin-bottom: 0;
+}
+</style>
