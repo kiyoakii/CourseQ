66bc8f06336c6220ced1c5f926abe985a85e5fbb
HD <wangshunhong@mail.ustc.edu.cn>
2020-05-12 22:33:56 +0800

    change the view of Admin pages

--- frontend/admin-app/src/views/AdminTeacherManage.vue
+++ frontend/admin-app/src/views/AdminTeacherManage.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <h2>教师名单</h2>
-    <div id="flex-bar">
-      <div class="flex-item">
-        <admin-member-list-search id="stu-search" identity="teacher"></admin-member-list-search>
-      </div>
-      <div class="flex-item">
-        <el-button type="primary" @click="onSubmit">注册新教师</el-button>
-      </div>
+  <div class="teacher-manage">
+    <div class="teacher-manage-header">
+      <h2>教师名单</h2>
     </div>
-    <admin-member-list id="stu-list"></admin-member-list>
+    <div class="teacher-manage-content">
+      <div id="flex-bar">
+        <div class="flex-item">
+          <admin-member-list-search id="stu-search" identity="teacher"></admin-member-list-search>
+        </div>
+        <div class="flex-item">
+          <el-button type="primary" @click="onSubmit">注册新教师</el-button>
+        </div>
+      </div>
+      <admin-member-list id="stu-list"></admin-member-list>
+    </div>
   </div>
 </template>
 
@@ -32,6 +36,19 @@ export default {
 </script>
 
 <style scoped>
+.teacher-manage {
+  flex: 1;
+  padding: 30px;
+}
+.teacher-manage-header {
+  padding: 0 100px;
+}
+.teacher-manage-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 200px;
+}
 #flex-bar {
   display: flex;
   flex-direction: row;
