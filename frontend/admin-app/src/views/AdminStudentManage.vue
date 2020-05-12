5861e6e0c8a83e54af1311a26cd822277d670f0a
HD <wangshunhong@mail.ustc.edu.cn>
2020-05-12 22:33:56 +0800

    change the view of Admin pages

--- frontend/admin-app/src/views/AdminStudentManage.vue
+++ frontend/admin-app/src/views/AdminStudentManage.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <h2>学生名单</h2>
-    <div id="flex-bar">
-      <div class="flex-item">
-        <admin-member-list-search id="stu-search" identity="student"></admin-member-list-search>
-      </div>
-      <div class="flex-item">
-        <el-button type="primary" @click="onSubmit">注册新学生</el-button>
-      </div>
+  <div class="student-manage">
+    <div class="student-manage-header">
+      <h2>学生名单</h2>
     </div>
-    <admin-member-list id="stu-list"></admin-member-list>
+    <div class="student-manage-content">
+      <div id="flex-bar">
+        <div class="flex-item">
+          <admin-member-list-search id="stu-search" identity="student"></admin-member-list-search>
+        </div>
+        <div class="flex-item">
+          <el-button type="primary" @click="onSubmit">注册新学生</el-button>
+        </div>
+      </div>
+      <admin-member-list id="stu-list"></admin-member-list>
+    </div>
   </div>
 </template>
 
@@ -32,6 +36,19 @@ export default {
 </script>
 
 <style scoped>
+.student-manage {
+  flex: 1;
+  padding: 30px;
+}
+.student-manage-header {
+  padding: 0 100px;
+}
+.student-manage-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 200px;
+}
 #flex-bar {
   display: flex;
   flex-direction: row;
