e82edc381eff7b0464824fc65fea2cdeb09fa035
HD <wangshunhong@mail.ustc.edu.cn>
2020-05-13 00:16:53 +0800

    change the view of Timeline

--- frontend/proans-app/src/views/TimeLine.vue
+++ frontend/proans-app/src/views/TimeLine.vue
@@ -4,30 +4,41 @@
     <div slot="header" class="clearfix">
       <span>版本时间轴</span>
     </div>
-    <el-scrollbar class= "timeline">
-      <ul class="timeline-list">
-        <li class="timeline-item" v-for="(item, i) in timelineList" :key="i"
-        @click="chooseVersion(item.versionId)">
-          <router-link :to="'./' + item.versionId">
-            <div v-if="i !== 0"
-            class="timeline-item-line timeline-item-line-left"></div>
-            <div v-if="i != timelineList.length - 1"
-            class="timeline-item-line timeline-item-line-right"></div>
-            <el-tooltip class="item" effect="dark"
-            :content="'用户： ' + item.userId + ' 进行的修改'" placement="top">
-              <div class="timeline-item-node"
-              :class="activeVersion === item.versionId ? 'timeline-item-node-active' : ''"></div>
-            </el-tooltip>
-            <div class="timeline-item-content">
-              <div class="timeline-item-desc">
-                {{ item.desc }}
+    <ul class="timeline-list">
+      <li class="timeline-item" v-for="(item, i) in timelineList" :key="i">
+        <div v-if="i !== 0"
+          class="timeline-item-line timeline-item-line-left"></div>
+        <div v-if="i != timelineList.length - 1"
+          class="timeline-item-line timeline-item-line-right"></div>
+        <router-link :to="'./' + item.versionId">
+          <el-tooltip class="item" effect="light" placement="top">
+            <div slot="content">
+              <div class="timeline-item-content">
+                <span class="timeline-item-desc">
+                  {{ item.userId + '进行的修改：' }}
+                </span>
+                <div class="timeline-item-desc">
+                  {{ item.desc }}
+                </div>
+                <div class="timeline-item-time">{{ item.time }}</div>
               </div>
-              <div class="timeline-item-time">{{ item.time }}</div>
             </div>
-          </router-link>
-        </li>
-      </ul>
-    </el-scrollbar>
+            <div class="timeline-item-node"
+              @click="chooseVersion(item.versionId)"
+              :class="activeVersion === item.versionId ? 'timeline-item-node-active' : ''"></div>
+          </el-tooltip>
+        </router-link>
+        <router-link :to="'./' + item.versionId + '/diff'">
+          <el-tooltip class="item" effect="light" placement="top"
+            v-if="i != timelineList.length - 1">
+            <div slot="content">
+              点击查看两个版本的差别
+            </div>
+            <i class="el-icon-document-copy"></i>
+          </el-tooltip>
+        </router-link>
+      </li>
+    </ul>
   </el-card>
 </template>
 
@@ -102,6 +113,7 @@ export default {
 
 a {
   text-decoration: none;
+  color: black;
 }
 
 .timeline-container {
@@ -109,21 +121,16 @@ a {
   justify-content: center;
   /* align-items: center; */
 }
-.timeline {
-  display: flex;
-  position: relative;
-  justify-content: center;
-  width: 100%;
-}
 
 .timeline-list {
   display: flex;
+  height: 50px;
 }
 
 .timeline-item {
   position: relative;
   display: flex;
-  width: 200px;
+  width: 100px;
   flex-shrink: 0;
   justify-items: baseline;
 }
@@ -134,9 +141,22 @@ a {
   display: flex;
   justify-content: center;
   align-items: center;
-  left: 50%;
+  top: 15px;
+  left: calc(50% - 10px);
   width: 20px;
   height: 20px;
+  cursor: pointer;
+}
+.el-icon-document-copy {
+  display: none;
+}
+ul:hover .el-icon-document-copy {
+  display: flex;
+  position: absolute;
+  top: 15px;
+  right: -10px;
+  z-index: 1;
+  font-size: 16px;
   cursor: pointer;
 }
 .timeline-item-node-active {
@@ -146,7 +166,7 @@ a {
 .timeline-item-line {
   position: absolute;
   width: 50%;
-  top: 10px;
+  top: 25px;
   border-top: 2px solid #e4e7ed;
 }
 
@@ -155,15 +175,11 @@ a {
 }
 
 .timeline-item-line-right {
-  right: 0;
-}
-
-.timeline-prev, .timeline-next {
+  left: 50%;
 }
 
 .timeline-item-content {
   width: 100%;
-  padding: 28px 10px 0 10px;
   color: black;
 }
 
