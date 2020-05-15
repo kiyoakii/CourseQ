8124aa0943aa88afa8fc5d5323653f3d2f71c6ff
biuren <biuren@mail.ustc.edu.cn>
2020-05-16 02:21:52 +0800

    backend: rename id

--- backend/course/app/models/answer.py
+++ backend/course/app/models/answer.py
@@ -5,7 +5,7 @@ from app.models.base import Base
 
 class Answer(Base):
     ansid = Column(Integer, primary_key=True)
-    qid = Column(Integer, ForeignKey('question.qid'))
+    qid = Column(Integer, ForeignKey('question.id'))
     content = Column(String(500), nullable=False)
     author = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
