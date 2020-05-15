501f030b747ebd9c381851ca36ba06af46228ec7
biuren <biuren@mail.ustc.edu.cn>
2020-05-16 02:23:28 +0800

    backend: add title and fix relationships

--- backend/course/app/models/question.py
+++ backend/course/app/models/question.py
@@ -2,14 +2,16 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 from app.models.base import Base
+from app.models.relation import question_tag_table
 
 
 class Question(Base):
-    qid = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(127), nullable=False)
     content = Column(Text)
     author_id = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
     answer = relationship('Answer', backref='question')
     discussions = relationship('Discussion', backref='question')
     course_id = Column(Integer, ForeignKey('course.cid'))
-    tags = relationship('Tag', backref='question', secondary='QuestionTag')
+    tags = relationship('Tag', backref='question', secondary=question_tag_table)
