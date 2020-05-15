eff094d7d0d4cfaf72310652ce5c4181a570ebc3
biuren <biuren@mail.ustc.edu.cn>
2020-05-16 02:24:01 +0800

    backend: rename relation

--- backend/course/app/models/__init__.py
+++ backend/course/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .answer import Answer
 from .course import Course
 from .discussion import Discussion
-from .question import Question
 from .user import User
 from .tag import Tag
 from .resource import CourseResource
+import app.models.relation
