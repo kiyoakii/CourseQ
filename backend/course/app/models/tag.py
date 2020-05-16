36b6ddb09e2c0636a12a0a0b03f016d3352da366
biuren <biuren@mail.ustc.edu.cn>
2020-05-16 18:15:21 +0800

    backend: add a static method

--- backend/course/app/models/tag.py
+++ backend/course/app/models/tag.py
@@ -1,9 +1,18 @@
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from app.models.base import Base, db
 
 
 class Tag(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(20), unique=True)
+
+    @staticmethod
+    def get_or_create_tag(tag_name):
+        tag_set = Tag.query.filter_by(name=tag_name)
+        if tag_set.count() == 0:
+            tag = Tag(name=tag_name)
+        else:
+            tag = tag_set.first()
+        return tag
