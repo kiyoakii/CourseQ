d3faf4bcca5be5628ee77d16e6b4676e68c4e5dd
biuren <biuren@mail.ustc.edu.cn>
2020-05-16 18:15:49 +0800

    backend: add reconstructor

--- backend/course/app/models/question.py
+++ backend/course/app/models/question.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, reconstructor
 
 from app.models.base import Base
 from app.models.relation import question_tag_table
@@ -8,10 +8,15 @@ from app.models.relation import question_tag_table
 class Question(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(127), nullable=False)
-    content = Column(Text)
+    content = Column(Text, nullable=False)
     author_id = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
     answer = relationship('Answer', backref='question')
     discussions = relationship('Discussion', backref='question')
     course_id = Column(Integer, ForeignKey('course.cid'))
     tags = relationship('Tag', backref='question', secondary=question_tag_table)
+
+    @reconstructor
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields = ['id', 'title']
