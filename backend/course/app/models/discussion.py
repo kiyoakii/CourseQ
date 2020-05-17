a870d2f544f5b277b30af3b6e2d47a5745241353
biuren <biuren@mail.ustc.edu.cn>
2020-05-18 01:31:35 +0800

    backend: rename some fields

--- backend/course/app/models/discussion.py
+++ backend/course/app/models/discussion.py
@@ -4,8 +4,8 @@ from app.models.base import Base
 
 
 class Discussion(Base):
-    did = Column(Integer, primary_key=True)
-    qid = Column(Integer, ForeignKey('question.id'))
+    id = Column(Integer, primary_key=True)
+    question = Column(Integer, ForeignKey('question.id'))
     content = Column(String(500), nullable=False)
     author = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
