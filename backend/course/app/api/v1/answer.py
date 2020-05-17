5d2cf8d9c3dc8a0f5734d24f18eca2f5ad8905ed
biuren <biuren@mail.ustc.edu.cn>
2020-05-18 01:30:18 +0800

    backend: add answer methods

--- backend/course/app/api/v1/answer.py
+++ backend/course/app/api/v1/answer.py
@@ -1,3 +1,31 @@
 from app.libs.redprint import Redprint
+from app.validators.forms import AnswerUpdateForm
+from app.models.answer import Answer
+from app.models.base import db
+from app.libs.error_code import Success, DeleteSuccess
+from flask import jsonify
 
 api = Redprint('answer')
+
+
+@api.route('/<int:aid>/update', methods=['PUT'])
+def update_answer(aid):
+    answer = Answer.query.get_or_404(aid)
+    form = AnswerUpdateForm().validate_for_api()
+    with db.auto_commit():
+        form.populate_obj(answer)
+    return Success()
+
+
+@api.route('/<int:aid>', methods=['GET'])
+def get_answer(aid):
+    answer = Answer.query.get_or_404(aid)
+    return jsonify(answer)
+
+
+@api.route('/<int:aid>', methods=['DELETE'])
+def delete_answer(aid):
+    answer = Answer.query.get_or_404(aid)
+    with db.auto_commit():
+        answer.delete()
+    return DeleteSuccess()
