9eae9805e091c5af7655fc34fe43dcbfe5826aca
biuren <biuren@mail.ustc.edu.cn>
2020-05-18 11:50:35 +0800

    backend: new discussion answer class

--- backend/course/app/models/discussion.py
+++ backend/course/app/models/discussion.py
@@ -1,11 +1,21 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
-
+from sqlalchemy.orm import relationship
 from app.models.base import Base
 
 
-class Discussion(Base):
+class DiscussionTopic(Base):
     id = Column(Integer, primary_key=True)
-    question = Column(Integer, ForeignKey('question.id'))
+    question_id = Column(Integer, ForeignKey('question.id'))
+    title = Column(String(127), nullable=False)
     content = Column(String(500), nullable=False)
-    author = Column(String(10), ForeignKey('user.gid'))
+    author_gid = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
+    topic_answer = relationship('DiscussionAnswer')
+
+
+class DiscussionAnswer(Base):
+    id = Column(Integer, primary_key=True)
+    topic_id = Column(Integer, ForeignKey(DiscussionTopic.id))
+    content = Column(String(500), nullable=False)
+    author_gid = Column(String(10), ForeignKey('user.gid'))
+    reply_id = Column(Integer, default=0)
