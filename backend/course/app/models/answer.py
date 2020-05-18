d2f6b9b349232e6e83638625e09b4e83771e2dd6
biuren <biuren@mail.ustc.edu.cn>
2020-05-18 11:07:33 +0800

    backend: rename some foreign_key

--- backend/course/app/models/answer.py
+++ backend/course/app/models/answer.py
@@ -5,9 +5,9 @@ from app.models.base import Base
 
 class Answer(Base):
     id = Column(Integer, primary_key=True)
-    question = Column(Integer, ForeignKey('question.id'))
+    question_id = Column(Integer, ForeignKey('question.id'))
     content = Column(String(500), nullable=False)
-    author = Column(String(10), ForeignKey('user.gid'))
+    author_gid = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
 
     @reconstructor
--- backend/course/app/models/question.py
+++ backend/course/app/models/question.py
@@ -9,14 +9,14 @@ class Question(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(127), nullable=False)
     content = Column(Text, nullable=False)
-    author = Column(String(10), ForeignKey('user.gid'))
+    author_gid = Column(String(10), ForeignKey('user.gid'))
     stars = Column(Integer, default=0)
-    answer = relationship('Answer', backref='question')
-    discussions = relationship('Discussion', backref='question')
+    answer = relationship('Answer')
+    discussions = relationship('Discussion')
     course_id = Column(Integer, ForeignKey('course.cid'))
     tags = relationship('Tag', backref='question', secondary=question_tag_table)
 
     @reconstructor
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields = ['id', 'title', 'starts', 'tags']
+        self.fields = ['id', 'title', 'stars', 'tags']
