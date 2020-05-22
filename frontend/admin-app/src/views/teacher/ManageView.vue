4b82fc258ef0edc401d0cee6e4860c7810b67f21
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-22 17:51:19 +0800

    new teacher manage view entrance

---
+++ frontend/admin-app/src/views/teacher/ManageView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-container>
+      <side-bar></side-bar>
+      <router-view></router-view>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue';
+
+export default {
+  name: 'ManageView',
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<style>
+
+</style>
