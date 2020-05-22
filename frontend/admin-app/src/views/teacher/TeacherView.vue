cc1c86d63a1057566df1814707bb898fceb484a3
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-22 17:50:07 +0800

    new teacher view

---
+++ frontend/admin-app/src/views/teacher/TeacherView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <teacher-header></teacher-header>
+      </el-header>
+      <el-main>
+        <el-row type="flex" justify="center">
+          <el-col :xs="24" :sm="24" :md="22" :lg="22" :xl="22" >
+            <teacher-content></teacher-content>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import TeacherHeader from '@/views/teacher/TeacherHeader.vue';
+import TeacherContent from '@/views/teacher/TeacherContent.vue';
+
+export default {
+  name: 'TeacherView',
+  components: {
+    TeacherHeader,
+    TeacherContent,
+  },
+};
+</script>
+
+<style>
+
+</style>
