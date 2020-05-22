e6288e941f0e1e10da654df3fbec0fd84b0477e5
biuren <biuren@mail.ustc.edu.cn>
2020-05-23 02:21:18 +0800

    backend: add like function

--- backend/course/app/api/v1/answer.py
+++ backend/course/app/api/v1/answer.py
@@ -2,6 +2,8 @@ from app.libs.redprint import Redprint
 from app.validators.forms import AnswerForm
 from app.models.answer import Answer
 from app.models.base import db
+from app.models.upvote import AnswerUpVote
+from app.libs.error_code import CancelUpVoteSuccess, UpVoteSuccess
 from app.libs.error_code import Success, DeleteSuccess
 from flask import jsonify
 
@@ -29,3 +31,29 @@ def delete_answer(aid):
     with db.auto_commit():
         answer.delete()
     return DeleteSuccess()
+
+
+@api.route('/<int:qid>/like', methods=['POST'])
+def up_vote(qid):
+    answer = Answer.query.get_or_404(qid)
+    answer_up_vote = AnswerUpVote.query \
+        .filter_by(answer_id=answer.id) \
+        .filter_by(user_gid='0000000000')
+    if answer_up_vote.count():
+        with db.auto_commit():
+            answer_up_vote.first().delete()
+        return CancelUpVoteSuccess()
+    else:
+        with db.auto_commit():
+            answer_up_vote = AnswerUpVote(
+                answer_id=answer.id,
+                user_gid='0000000000'
+            )
+            db.session.add(answer_up_vote)
+        return UpVoteSuccess()
+
+
+@api.route('/<int:qid>/like', methods=['GET'])
+def get_vote_num(qid):
+    answer = Answer.query.get_or_404(qid)
+    return jsonify({'likes': sum(map(lambda vote: vote.status == 1, answer.up_votes))})
