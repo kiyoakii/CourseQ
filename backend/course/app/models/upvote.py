083c5f546d3da345f77d56b89d13e9139b3ab5b3
biuren <biuren@mail.ustc.edu.cn>
2020-05-23 01:55:29 +0800

    backend: new model for up vote

---
+++ backend/course/app/models/upvote.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, LargeBinary
+from sqlalchemy.orm import reconstructor
+
+from app.models.base import Base
+
+
+class AnswerUpVote(Base):
+    id = Column(Integer, primary_key=True)
+    answer_id = Column(Integer, ForeignKey('answer.id'))
+    user_gid = Column(String(10), ForeignKey('user.gid'))
+
+
+class QuestionUpVote(Base):
+    id = Column(Integer, primary_key=True)
+    question_id = Column(Integer, ForeignKey('question.id'))
+    user_gid = Column(String(10), ForeignKey('user.gid'))
