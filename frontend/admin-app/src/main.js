5d645e7fbb125cf75a8bc7fa43fbf542e1e34ea9
zsStrike <2439758713@qq.com>
2020-05-25 20:59:57 +0800

    admin: add axios and vue-axios

--- frontend/admin-app/src/main.js
+++ frontend/admin-app/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
