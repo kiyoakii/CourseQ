cc45f42c916c63a5a7cdc3d3449c3a8d7c0bccf1
zsStrike <2439758713@qq.com>
2020-05-26 14:06:36 +0800

    proans: add axios & mockjs

--- frontend/proans-app/src/main.js
+++ frontend/proans-app/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element';
-import './plugins/vue-mavon-editor';
+import './mock';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
