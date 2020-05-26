bacd9184df2394de7d1c6df52bed38d1b25778a8
zsStrike <2439758713@qq.com>
2020-05-26 13:51:07 +0800

    admin: add mock data

--- frontend/admin-app/src/main.js
+++ frontend/admin-app/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element';
+import './mock';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
