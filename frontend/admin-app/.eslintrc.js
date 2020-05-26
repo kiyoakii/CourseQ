4e5c09e096ea69c7e8b78b2f5393a744366d33c2
zsStrike <2439758713@qq.com>
2020-05-26 13:50:05 +0800

    admin: fix import/no-extraneous-dependencies problem

--- frontend/admin-app/.eslintrc.js
+++ frontend/admin-app/.eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/no-extraneous-dependencies': ['warn', {
+      'optionalDependencies': ['src/mock/index.js'],
+    }],
   },
   overrides: [
     {
