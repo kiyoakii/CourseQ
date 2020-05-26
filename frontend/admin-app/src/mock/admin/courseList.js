b92df577d7cadc1dc1ec1991331110c9652fcb0c
zsStrike <2439758713@qq.com>
2020-05-26 13:59:17 +0800

    admin: change double-quotes to single-quotes

--- frontend/admin-app/src/mock/admin/courseList.js
+++ frontend/admin-app/src/mock/admin/courseList.js
@@ -3,13 +3,13 @@ import Mock from 'mockjs';
 const courses = {
   'courses|2-7': [
     {
-      "courseName|+1": ["计算方法", "体系结构", "信息安全", "微积分", "算法基础", "代数结构", "图论"],
-      "teacherName|1": () => Mock.Random.cname(),
-      "id": () => Mock.Random.id(),
+      'courseName|+1': ['计算方法', '体系结构', '信息安全', '微积分', '算法基础', '代数结构', '图论'],
+      'teacherName|1': () => Mock.Random.cname(),
+      id: () => Mock.Random.id(),
     },
-  ]
+  ],
 };
 
 export default {
-  courses
-}
+  courses,
+};
