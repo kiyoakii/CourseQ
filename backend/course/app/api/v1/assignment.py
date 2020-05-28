34e4afaf7f8f7eaba4b6a6afddb244c05627ef1a
biuren <biuren@mail.ustc.edu.cn>
2020-05-29 01:14:58 +0800

    backend: add schedule and assignment basic methods

--- backend/course/app/api/v1/assignment.py
+++ backend/course/app/api/v1/assignment.py
@@ -1,12 +1,28 @@
 from io import BytesIO
 
-from flask import request, send_file
+from flask import request, send_file, jsonify
 
 from app.libs.error_code import Success, Forbidden, ParameterException, FileSuccess
 from app.libs.redprint import Redprint
 from app.models.base import db
-from app.models.course import Course
+from app.models.assignment import Assignment
+from app.validators.forms import AssignmentUpdateForm
 from app.models.resource import CourseResource
 from app.validators.forms import ResourceForm
 
 api = Redprint('assignment')
+
+
+@api.route('/<int:aid>', methods=['GET'])
+def get_assignment(aid):
+    assignment = Assignment.query.get_or_404(aid)
+    return jsonify(assignment)
+
+
+@api.route('/<int:aid>', methods=['POST'])
+def update_assignment(aid):
+    assignment = Assignment.query.get_or_404(aid)
+    form = AssignmentUpdateForm().validate_for_api()
+    with db.auto_commit():
+        form.populate_obj(assignment)
+    return Success()
