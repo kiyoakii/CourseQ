3fd43b7b9badbe20c93a4a6dd759f5dc560d5f60
zsStrike <2439758713@qq.com>
2020-05-29 20:48:05 +0800

    admin: update mock index.js

--- frontend/admin-app/src/mock/index.js
+++ frontend/admin-app/src/mock/index.js
@@ -1,4 +1,8 @@
 import Mock from 'mockjs';
-import courses from './admin/courseList';
+import courseList from './admin/courseList';
+import teacherList from './admin/teacherList';
+import studentList from './admin/studentList';
 
-Mock.mock('/v1/admin/admin/course-list', 'get', courses.courses);
+Mock.mock('/v1/admin/admin/course-list', 'get', courseList);
+Mock.mock('/v1/admin/admin/teacher-list', 'get', teacherList);
+Mock.mock('/v1/admin/admin/student-list', 'get', studentList);
