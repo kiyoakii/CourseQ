9474ee158a9ef6814502caef73edda8cd3a6cfc0
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-31 13:51:51 +0800

    proans: add SideBar.vue

---
+++ frontend/proans-app/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <search></search>
+    <problem-list></problem-list>
+  </div>
+</template>
+
+<script>
+import ProblemList from '@/components/ProblemList.vue';
+import Search from '@/components/Search.vue';
+
+export default {
+  name: 'SideBar',
+  components: {
+    ProblemList,
+    Search,
+  },
+};
+</script>
+
+<style>
+
+</style>
