149944c9122d6df358bd5f2248bebe362e550901
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-31 14:08:14 +0800

    proans: SideBar's mock and axios

--- frontend/proans-app/src/components/ProblemList.vue
+++ frontend/proans-app/src/components/ProblemList.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
-      <li v-for="(problem, index) in plist" :key=" problem.id " class="infinite-list-item">
+      <li v-for="(problem, index) in problems" :key="index " class="infinite-list-item">
         <el-card class="box-card" shadow="hover">
           <div slot="header" class="clearfix">
-            <router-link :to="'/proans/1/problem/' + index" class="link">
+            <router-link :to="'/proans/1/problem/' + problem.id" class="link">
               <span>{{ problem.title }}</span>
             </router-link>
           </div>
           <div class="text item">
-            {{ problem.desc | formatDesc }}
+            {{ problem.content | formatDesc }}
           </div>
         </el-card>
       </li>
@@ -20,41 +20,12 @@
 <script>
 export default {
   name: 'ProblemList',
+  props: {
+    problems: [],
+  },
   data() {
     return {
       count: 0,
-      plist: [
-        {
-          id: 0,
-          title: '动态规划',
-          desc: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
-                 规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
-        },
-        {
-          id: 1,
-          title: '贪心算法',
-          desc: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
-                 规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
-        },
-        {
-          id: 2,
-          title: '贪心算法1',
-          desc: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
-                 规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
-        },
-        {
-          id: 3,
-          title: '贪心算法2',
-          desc: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
-                 规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
-        },
-        {
-          id: 4,
-          title: '贪心算法3',
-          desc: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
-                 规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
-        },
-      ],
     };
   },
   methods: {
