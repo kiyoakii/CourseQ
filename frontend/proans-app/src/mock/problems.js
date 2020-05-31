4f73ddc9e715a5a56fac47000300bf8d718b8219
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-31 14:08:14 +0800

    proans: SideBar's mock and axios

---
+++ frontend/proans-app/src/mock/problems.js
@@ -0,0 +1,20 @@
+const problems = {
+  code: 0,
+  msg: '...',
+  data: {
+    problems: [
+      {
+        id: '123123',
+        title: 'Problem1',
+        content: 'Description...',
+      },
+      {
+        id: '321111',
+        title: 'Problem2',
+        content: 'Description...',
+      },
+    ],
+  },
+};
+
+export default problems;
