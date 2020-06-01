296ad7ea48cc54f007a5440e445b033213e76c80
HD <wangshunhong@mail.ustc.edu.cn>
2020-06-01 23:30:44 +0800

    CourseListView.vue & SemesterListView.vue: mock and axios

--- frontend/admin-app/src/mock/index.js
+++ frontend/admin-app/src/mock/index.js
@@ -2,7 +2,14 @@ import Mock from 'mockjs';
 import courseList from './admin/courseList';
 import teacherList from './admin/teacherList';
 import studentList from './admin/studentList';
+import tCourseList from './teacher/tCourseList';
+import semesterList from './teacher/semesterList';
+import allInfo from './teacher/allInfo';
+
 
 Mock.mock('/v1/admin/admin/course-list', 'get', courseList);
 Mock.mock('/v1/admin/admin/teacher-list', 'get', teacherList);
 Mock.mock('/v1/admin/admin/student-list', 'get', studentList);
+Mock.mock('/v1/admin/teacher/course-list', 'get', tCourseList);
+Mock.mock('/v1/admin/teacher/semester-list?course=1', 'get', semesterList);
+Mock.mock('/v1/admin/teacher/course/allinfo?course=1&semester=1', 'get', allInfo);
--- frontend/admin-app/src/views/teacher/ManageView.vue
+++ frontend/admin-app/src/views/teacher/ManageView.vue
@@ -2,7 +2,7 @@
   <div>
     <el-container>
       <side-bar></side-bar>
-      <router-view></router-view>
+      <router-view :allInfo="allInfo"></router-view>
     </el-container>
   </div>
 </template>
@@ -14,6 +14,22 @@ export default {
   name: 'ManageView',
   components: {
     SideBar,
+  },
+  data() {
+    return {
+      allInfo: {},
+    };
+  },
+  mounted() {
+    this.axios.get('/v1/admin/teacher/course/allinfo?course=1&semester=1')
+      .then((res) => {
+        console.log(res);
+        if (res.status !== 200) {
+          console.log(JSON.stringify(res.data));
+          return;
+        }
+        this.allInfo = res.data.allInfo;
+      });
   },
 };
 </script>
