c77705fdf630153457dd4b8c64d9659e80c37b5f
Jin <lijin110110@gmail.com>
2020-06-02 17:29:50 +0800

    update decorator

--- backend/course/app/api/v1/user.py
+++ backend/course/app/api/v1/user.py
@@ -1,9 +1,10 @@
-from flask import jsonify, g, current_app
+from flask import jsonify, g, current_app, Request
+from flask_jwt_extended import jwt_required, create_access_token
 
-from app.api.v1.token import generate_auto_token
+from app.libs.enums import UserTypeEnum
 from app.libs.error_code import DeleteSuccess, RegisterSuccess
 from app.libs.redprint import Redprint
-from app.libs.token_auth import auth
+from app.libs.token_auth import role_required, login_required
 from app.models.base import db
 from app.models.user import User
 from app.validators.forms import UserForm
@@ -12,14 +13,14 @@ api = Redprint('user')
 
 
 @api.route('/<string:gid>', methods=['GET'])
-@auth.login_required
+@role_required(UserTypeEnum.MANAGER)
 def super_get_user(gid):
     user = User.query.filter_by(gid=gid).first_or_404()
     return jsonify(user)
 
 
 @api.route('', methods=['GET'])
-@auth.login_required
+@login_required
 def get_user():
     gid = g.user.gid
     user = User.query.filter_by(gid=gid).first_or_404()
@@ -27,7 +28,6 @@ def get_user():
 
 
 @api.route('', methods=['DELETE'])
-@auth.login_required
 def delete_user():
     gid = g.user.gid
     with db.auto_commit():
@@ -37,7 +37,7 @@ def delete_user():
 
 
 @api.route('/register', methods=['POST'])
-@auth.login_required
+@login_required
 def register():
     form = UserForm().validate_for_api()
     user = User().register(form.nickname.data,
