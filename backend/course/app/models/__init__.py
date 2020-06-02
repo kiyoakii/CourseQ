e517824b54b58d12f2919521e24fa522195e3fec
Jin <lijin110110@gmail.com>
2020-06-02 17:32:29 +0800

    imports optimization

--- backend/course/app/models/__init__.py
+++ backend/course/app/models/__init__.py
@@ -1,8 +1,9 @@
+import app.models.relation
 from .answer import Answer
 from .course import Course
 from .discussion import DiscussionTopic, DiscussionAnswer
-from .user import User
-from .tag import Tag
 from .resource import CourseResource
 from .assignment import Assignment
 import app.models.relation
+from .tag import Tag
+from .user import User
