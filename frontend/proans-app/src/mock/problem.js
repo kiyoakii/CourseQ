f7e1c278e31a26b36f98968b509b93eeecd2b2c3
nnnyt <793313994@qq.com>
2020-06-07 20:37:44 -0700

    proans:ProblemCard's mock & axios

---
+++ frontend/proans-app/src/mock/problem.js
@@ -0,0 +1,17 @@
+const problem = {
+  code: 0,
+  msg: '...',
+  data: {
+    problem: {
+      id: '1',
+      title: '动态规划',
+      tags: ['标签一', '标签二', '标签三'],
+      content: `动态规划相信大家都知道，但是动态规划到底是什么呢，下面小编给大家介绍一下吧。动态
+            规划就是一种算法，怎么样，大家有什么想法可以在评论区写下来一起分享。`,
+      stared: false,
+      packed: false,
+    },
+  },
+};
+
+export default problem;
