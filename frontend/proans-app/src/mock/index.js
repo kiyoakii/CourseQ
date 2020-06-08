9673164c3a81924f62d9bc3ff5a18e285ce0eee4
nnnyt <793313994@qq.com>
2020-06-07 19:59:24 -0700

    proans: ProblemCategories' mock & axios

--- frontend/proans-app/src/mock/index.js
+++ frontend/proans-app/src/mock/index.js
@@ -1,5 +1,7 @@
 import Mock from 'mockjs';
 import problems from './problems';
+import categories from './categories';
 
 Mock.mock('/v1/proans/problems?id=1', 'get', problems);
 Mock.mock('/v1/proans/problems?id=1&category=2', 'get', problems);
+Mock.mock('/v1/proans/categories?id=1', 'get', categories);
