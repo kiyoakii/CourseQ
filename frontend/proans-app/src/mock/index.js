61e5bc6125d7fe6f415d75a9740e63834712bad1
nnnyt <793313994@qq.com>
2020-06-07 20:54:28 -0700

    proans:Discussion's mock & axios

--- frontend/proans-app/src/mock/index.js
+++ frontend/proans-app/src/mock/index.js
@@ -3,6 +3,7 @@ import problems from './problems';
 import categories from './categories';
 import problem from './problem';
 import answer from './answer';
+import discussions from './discussions';
 
 Mock.mock('/v1/proans/problems?id=1', 'get', problems);
 Mock.mock('/v1/proans/problems?id=1&category=2', 'get', problems);
@@ -10,3 +11,4 @@ Mock.mock('/v1/proans/categories?id=1', 'get', categories);
 Mock.mock('/v1/proans/problem/detail?id=1&problem=1', 'get', problem);
 Mock.mock('/v1/proans/answer/teacher?id=1&problem=1', 'get', answer.teacher);
 Mock.mock('/v1/proans/answer/student?id=1&problem=1', 'get', answer.student);
+Mock.mock('/v1/proans/discussions?id=1&problem=1', 'get', discussions);
