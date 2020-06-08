79096ae5b6b024357911e39e6c9810e7e5f5660b
nnnyt <793313994@qq.com>
2020-06-07 19:59:24 -0700

    proans: ProblemCategories' mock & axios

---
+++ frontend/proans-app/src/mock/categories.js
@@ -0,0 +1,18 @@
+const categories = {
+  code: 0,
+  msg: '...',
+  data: {
+    categories: [
+      {
+        id: '1',
+        name: 'HW1',
+      },
+      {
+        id: '2',
+        name: 'HW2',
+      },
+    ],
+  },
+};
+
+export default categories;
