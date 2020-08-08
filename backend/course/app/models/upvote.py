f306245d2be7714aa98672585ffcad9cf8168ea9
biuren <biuren@mail.ustc.edu.cn>
2020-08-09 03:21:47 +0800

    backend: remove answer upVote

--- backend/course/app/models/upvote.py
+++ backend/course/app/models/upvote.py
@@ -3,12 +3,6 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from app.models.base import Base
 
 
-class AnswerUpVote(Base):
-    id = Column(Integer, primary_key=True)
-    answer_id = Column(Integer, ForeignKey('answer.id'))
-    user_gid = Column(String(10), ForeignKey('user.gid'))
-
-
 class QuestionUpVote(Base):
     id = Column(Integer, primary_key=True)
     question_id = Column(Integer, ForeignKey('question.id'))
