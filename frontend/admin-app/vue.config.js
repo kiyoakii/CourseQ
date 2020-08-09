f2099a9f122c057326082ee9d67b7d06d9df0b7a
zsStrike <2439758713@qq.com>
2020-08-09 12:31:32 +0800

    admin: update vue dev server

---
+++ frontend/admin-app/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://27.102.70.185:8080',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
