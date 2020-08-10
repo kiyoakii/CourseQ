20e62aa8931b18603ebf697e2c11630979435d04
zsStrike <2439758713@qq.com>
2020-08-10 19:38:07 +0800

    feat: using vuex to store all problems, and add examples in App.vue

--- frontend/proans-app/src/store/index.js
+++ frontend/proans-app/src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    problems: [],
+    count: 0,
+  },
+  getters: {
+    allProblems(state) {
+      return state.problems;
+    },
   },
   mutations: {
+    initProblems(state, problems) {
+      state.problems.splice(0, state.problems.length);
+      state.problems.push(...problems);
+    },
   },
   actions: {
+    initProblems(context) {
+      axios.get('/api/v1/courses/3/questions')
+        .then((res) => {
+          if (res.status === 200) {
+            context.commit('initProblems', res.data);
+          }
+        });
+    },
   },
   modules: {
   },
