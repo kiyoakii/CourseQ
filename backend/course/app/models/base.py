ffdb651c6bb8cfb6e992f23adf14b965bd4f5f75
biuren <biuren@mail.ustc.edu.cn>
2020-08-12 02:19:11 +0800

    backend: implement a better soft query & fix create_time initial

--- backend/course/app/models/base.py
+++ backend/course/app/models/base.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy, BaseQuery
 from sqlalchemy import Column, Integer, SmallInteger
 
-from app.libs.error_code import NotFound
-
 
 class SQLAlchemy(_SQLAlchemy):
     @contextmanager
@@ -18,36 +16,46 @@ class SQLAlchemy(_SQLAlchemy):
             raise e
 
 
-class Query(BaseQuery):
-    def filter_by(self, **kwargs):
-        if 'status' not in kwargs.keys():
-            kwargs['status'] = 1
-        return super(Query, self).filter_by(**kwargs)
+class QueryWithSoftDelete(BaseQuery):
+    _with_deleted = False
 
-    def get_or_404(self, ident):
-        rv = self.get(ident)
-        if not rv:
-            raise NotFound()
-        return rv
+    def __new__(cls, *args, **kwargs):
+        obj = super(QueryWithSoftDelete, cls).__new__(cls)
+        obj._with_deleted = kwargs.pop('_with_deleted', False)
+        if len(args) > 0:
+            super(QueryWithSoftDelete, obj).__init__(*args, **kwargs)
+            return obj.filter_by(status=1) if not obj._with_deleted else obj
+        return obj
 
-    def first_or_404(self):
-        rv = self.first()
-        if not rv:
-            raise NotFound()
-        return rv
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def with_deleted(self):
+        return self.__class__(db.class_mapper(self._mapper_zero().class_),
+                              session=db.session(), _with_deleted=True)
+
+    def _get(self, *args, **kwargs):
+        # this calls the original query.get function from the base class
+        return super(QueryWithSoftDelete, self).get(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        # the query.get method does not like it if there is a filter clause
+        # pre-loaded, so we need to implement it using a workaround
+        obj = self.with_deleted()._get(*args, **kwargs)
+        return obj if obj is None or self._with_deleted or not obj.deleted else None
 
 
-db = SQLAlchemy(query_class=Query)
+db = SQLAlchemy()
 
 
 class Base(db.Model):
+    query_class = QueryWithSoftDelete
     __abstract__ = True
-    create_time = Column(Integer)
+    create_time = Column(Integer, default=int(datetime.now().timestamp()))
     status = Column(SmallInteger, default=1)
 
     def __init__(self, *args, **kwargs):
         super(Base, self).__init__(*args, **kwargs)
-        self.create_time = int(datetime.now().timestamp())
 
     def __getitem__(self, item):
         return getattr(self, item)
