013e9caf557741ad449f37e1b7bb6197df5a188c
biuren <biuren@mail.ustc.edu.cn>
2020-08-11 22:40:44 +0800

    backend: add announce

---
+++ backend/course/app/models/announce.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Text
+from sqlalchemy.orm import reconstructor, relationship
+
+from app.models.base import Base
+
+
+class Announce(Base):
+    id = Column(Integer, primary_key=True)
+    course_cid = Column(Integer, ForeignKey('course.cid'))
+    author_gid = Column(String(10), ForeignKey('user.gid'))
+    title = Column(String(63))
+    content = Column(Text, nullable=False)
+    author = relationship('User')
+
+    @reconstructor
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields = ['id', 'create_time', 'title', 'content', 'author']
