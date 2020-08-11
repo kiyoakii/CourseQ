1a6544a26bbd93365b21185aceb92efad27d8d22
biuren <biuren@mail.ustc.edu.cn>
2020-08-12 00:20:27 +0800

    backend: add announce

--- backend/course/app/api/v1/__init__.py
+++ backend/course/app/api/v1/__init__.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
 
-from app.api.v1 import user, token, course, resource, answer, question, discussion, topic_answer, schedule, assignment
+from app.api.v1 import user, token, course, resource, answer, question, discussion, topic_answer, schedule, assignment, \
+    announce
 
 
 def create_blueprint_v1():
     bp_v1 = Blueprint('v1', __name__)
-    user.api.register(bp_v1, url_prefix='/user')
+    user.api.register(bp_v1, url_prefix='/users')
     token.api.register(bp_v1, url_prefix='/token')
     course.api.register(bp_v1, url_prefix='/courses')
     resource.api.register(bp_v1, url_prefix='/resources')
@@ -15,4 +16,5 @@ def create_blueprint_v1():
     topic_answer.api.register(bp_v1, url_prefix='/topic_answers')
     schedule.api.register(bp_v1, url_prefix='/schedules')
     assignment.api.register(bp_v1, url_prefix='/assignments')
+    announce.api.register(bp_v1, url_prefix='/announces')
     return bp_v1
