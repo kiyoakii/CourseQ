532b4a8b09e759e425f65c57009a13dd6a9310ca
biuren <biuren@mail.ustc.edu.cn>
2020-08-11 16:31:31 +0800

    backend: add flask-migrate & init first migrate

---
+++ backend/course/manage.py
@@ -0,0 +1,20 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app import create_app
+from app.models.base import db
+
+app = create_app()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# 初始化 migrate
+# 两个参数一个是 Flask 的 app，一个是数据库 db
+migrate = Migrate(app, db)
+
+# 初始化管理器
+manager = Manager(app)
+# 添加 db 命令，并与 MigrateCommand 绑定
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
