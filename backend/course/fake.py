c4ddcbfd4da0cd6938909da346e0b503452de5b0
biuren <biuren@mail.ustc.edu.cn>
2020-08-12 16:08:12 +0800

    backend: more users

--- backend/course/fake.py
+++ backend/course/fake.py
@@ -6,11 +6,26 @@ from app.models.user import User
 app = create_app()
 with app.app_context():
     with db.auto_commit():
-        for i in range(100):
+        for i in range(100, 200):
             # 创建亿个超级管理员
             user = User()
             user.nickname = 'Super' + str(i)
             user.gid = '%010d' % i
             user.email = '999{0}@ustc.edu.cn'.format(i)
             user.auth = UserTypeEnum.MANAGER
+            db.session.add(user)
+        for i in range(200, 300):
+            # 创建亿个老师
+            user = User()
+            user.nickname = 'Super' + str(i)
+            user.gid = '%010d' % i
+            user.email = '999{0}@ustc.edu.cn'.format(i)
+            user.auth = UserTypeEnum.TEACHER
+            db.session.add(user)
+        for i in range(300, 400):
+            user = User()
+            user.nickname = 'Super' + str(i)
+            user.gid = '%010d' % i
+            user.email = '999{0}@ustc.edu.cn'.format(i)
+            user.auth = UserTypeEnum.STUDENT
             db.session.add(user)
