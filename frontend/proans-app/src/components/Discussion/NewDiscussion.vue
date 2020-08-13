f6f44a475ca6dc7fb0a2c1ff6b872c1fafd38b86
HD <wangshunhong@mail.ustc.edu.cn>
2020-08-13 21:34:22 +0800

    finish add new discussion

--- frontend/proans-app/src/components/Discussion/NewDiscussion.vue
+++ frontend/proans-app/src/components/Discussion/NewDiscussion.vue
@@ -4,23 +4,48 @@
       <div class="comment-header">
         <el-row type="flex" justify="space-between">
           <div class="comment-user">新的讨论</div>
-          <el-button type="primary" icon="el-icon-position" size="small"></el-button>
         </el-row>
       </div>
-      <div class="comment-body">
-        <editor></editor>
+      <div class="editor">
+        <div class="title">
+          <el-input placeholder="主题"
+          v-model="form.title"></el-input>
+        </div>
+        <mavon-editor v-model="form.content"></mavon-editor>
+        <div class="footer">
+          <el-button type="primary" icon="el-icon-position" size="small"
+          @click="onSubmit">提交</el-button>
+        </div>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
-import Editor from '../Editor.vue';
+// import Editor from '../Editor.vue';
 
 export default {
   name: 'NewDiscussion',
   components: {
-    Editor,
+    // Editor,
+  },
+  data() {
+    return {
+      form: {
+        title: '',
+        content: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.$route.query.qid);
+      this.axios.post(`/api/v1/questions/${this.$route.query.qid}/discussions`,
+        this.form)
+        .then((res) => {
+          console.log(res);
+        });
+    },
   },
 };
 </script>
@@ -39,4 +64,17 @@ export default {
   line-height: 30px;
   font-size: 15px;
 }
+
+.editor {
+  margin-top: 20px;
+}
+.title {
+  margin-bottom: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-end;
+}
+
 </style>
--- frontend/proans-app/src/components/Editor.vue
+++ frontend/proans-app/src/components/Editor.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
-    <!-- This is the Editor component. -->
-    <mavon-editor v-model="value"></mavon-editor>
+    <div class="title">
+      <el-input placeholder="主题"></el-input>
+    </div>
+    <mavon-editor @change="onChange" v-model="content"></mavon-editor>
+    <div class="footer">
+      <el-button type="primary" icon="el-icon-position" size="small">提交</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Editor',
+  model: {
+    prop: 'content',
+    event: 'submit',
+  },
   props: {
-    value: {
-      type: String,
-      default: '',
+  },
+  methods: {
+    onChange() {
+      console.log(this.content);
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.title {
 
-</style>
+}
+.footer {
+  display: flex;
+  justify-content: flex-end;
+
+}
+</style>>
