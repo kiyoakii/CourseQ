057369b52d4f1856d17432c1d2ae6ad829647789
zsStrike <2439758713@qq.com>
2020-08-14 22:53:01 +0800

    refactor: add user setting

--- frontend/admin-app/src/views/admin/AdminHeader.vue
+++ frontend/admin-app/src/views/admin/AdminHeader.vue
@@ -13,7 +13,13 @@
     </el-col>
     <el-col :span="2">
       <div class="center">
-        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        <el-dropdown placement="bottom" @command="commandHandler" size="medium">
+          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="profile">个人资料</el-dropdown-item>
+            <el-dropdown-item command="logout">登出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </div>
     </el-col>
   </el-row>
@@ -26,6 +32,11 @@ export default {
   name: 'AdminHeader',
   components: {
     AdminNavBar,
+  },
+  methods: {
+    commandHandler(command) {
+      console.log(command);
+    },
   },
 };
 </script>
