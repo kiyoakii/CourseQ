f336d6aec1186328e4684871a1eca82574436d04
zsStrike <2439758713@qq.com>
2020-08-14 21:06:31 +0800

    refactor: update data table

---
+++ frontend/admin-app/src/components/DataTableSearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-input
+      placeholder="请输入想要搜索的信息"
+      @input="(value) => $emit('input', value)"
+      :value="value"
+      prefix-icon="el-icon-search">
+      </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTableSearchBar',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-item {
+  margin-bottom: 0;
+}
+</style>
