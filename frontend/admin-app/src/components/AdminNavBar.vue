306b4ea123a7810748e9bf24c1a8777900360d34
zsStrike <2439758713@qq.com>
2020-08-14 21:07:21 +0800

    fix: correct the default active route

--- frontend/admin-app/src/components/AdminNavBar.vue
+++ frontend/admin-app/src/components/AdminNavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-menu default-active="/admin/1/courses" class="el-menu-demo"
-    mode="horizontal" router>
+    <el-menu :default-active="activeRoute" class="el-menu-demo"
+      mode="horizontal" router>
       <el-menu-item index="/admin/1/courses">课程管理</el-menu-item>
       <el-menu-item index="/admin/1/teachers">教师管理</el-menu-item>
       <el-menu-item index="/admin/1/students">学生管理</el-menu-item>
@@ -12,6 +12,11 @@
 <script>
 export default {
   name: 'AdminNavBar',
+  computed: {
+    activeRoute() {
+      return this.$router.history.current.fullPath;
+    },
+  },
 };
 </script>
 
