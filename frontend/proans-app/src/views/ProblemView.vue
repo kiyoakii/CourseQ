0a8e9405abdc5f03a07bfe0fb6c20fc23580bb5e
zsStrike <2439758713@qq.com>
2020-08-17 22:46:00 +0800

    feat: all data is computed

--- frontend/proans-app/src/views/ProblemView.vue
+++ frontend/proans-app/src/views/ProblemView.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="problem">
+    <time-line
+      :problemId="problemId"
+      @changeversion="changeVersion"
+      >
+    </time-line>
     <problem-card :problem="problem"
       @updateProblem="handleUpdateProblem"></problem-card>
     <answer-card :studentAnswer="studentAnswer"
       :teacherAnswer="teacherAnswer"
-      :problemId="problem.id"
+      :problemId="problemId"
       @updateProblem="handleUpdateProblem"></answer-card>
     <discussion :commentList="commentList"></discussion>
   </div>
@@ -14,6 +19,7 @@
 import ProblemCard from '@/components/ProblemCard.vue';
 import AnswerCard from '@/components/AnswerCard.vue';
 import Discussion from '@/components/Discussion.vue';
+import TimeLine from '../components/TimeLine.vue';
 
 export default {
   name: 'ProblemView',
@@ -21,53 +27,80 @@ export default {
     ProblemCard,
     AnswerCard,
     Discussion,
+    TimeLine,
   },
   data() {
     return {
-      problem: {},
-      studentAnswer: {},
-      teacherAnswer: {},
-      commentList: [],
+      versionId: -1,
     };
   },
+  computed: {
+    problem() {
+      const problem = this.$store.getters.problem(this.$store.state.qid);
+      if (this.versionId === -1) {
+        return problem;
+      }
+      if (!problem) {
+        return null;
+      }
+      if (this.versionId === problem.id) {
+        return problem;
+      }
+      console.log(problem);
+      const res = problem.history.find((item) => {
+        console.log(item.question && item.question.id, this.versionId);
+        return item.question && (item.question.id === this.versionId);
+      });
+      return {
+        ...res.question,
+        student_answer: res.student_answer,
+        teacher_answer: res.teacher_answer,
+      };
+    },
+    problemId() {
+      return this.$store.state.qid;
+    },
+    studentAnswer() {
+      return this.problem && this.problem.student_answer ? this.problem.student_answer : null;
+    },
+    teacherAnswer() {
+      return this.problem && this.problem.teacher_answer ? this.problem.teacher_answer : null;
+    },
+    commentList() {
+      return this.$store.getters.commentList || [];
+    },
+  },
   methods: {
-    handleUpdateProblem(qid) {
+    handleUpdateProblem() {
       this.$store.commit('updateProblem', this.problem);
-      setTimeout(() => {
-        this.problem = this.$store.getters.problem(qid);
-        this.studentAnswer = this.problem.student_answer;
-        this.teacherAnswer = this.problem.teacher_answer;
-      }, 400);
     },
     updateData() {
-      console.log(this.$route.params.qid);
-      setTimeout(() => {
-        this.problem = this.$store.getters.problem(this.$route.params.qid);
-        this.studentAnswer = this.problem.student_answer;
-        this.teacherAnswer = this.problem.teacher_answer;
-      }, 400);
-      this.axios.get(`/api/v1/questions/${this.$route.params.qid}/discussions`)
-        .then((res) => {
-          console.log(res);
-          if (res.status !== 200) {
-            console.log(JSON.stringify(res.data));
-            return;
-          }
-          this.commentList = res.data;
-        });
+    },
+    changeVersion({ id }) {
+      this.versionId = id;
     },
   },
   beforeRouteUpdate(to, from, next) {
     console.log(to, from);
     next();
-    if (this.$route.params.qid !== undefined) {
-      this.updateData();
-    }
+    this.versionId = -1;
+    this.$store.commit({
+      type: 'setQid',
+      id: this.$route.query.qid,
+    });
   },
-  beforeMount() {
-    if (this.$route.params.qid !== undefined) {
-      this.updateData();
-    }
+  beforeUpdate() {
+    this.$store.commit({
+      type: 'setQid',
+      id: this.$route.query.qid,
+    });
+  },
+  beforeCreate() {
+    this.$store.commit({
+      type: 'setQid',
+      id: this.$route.query.qid,
+    });
+    this.$store.dispatch('setCommentList');
   },
 };
 </script>
