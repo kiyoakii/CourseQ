3b867b214c905767512f85881bd8f57683be8483
biuren <biuren@mail.ustc.edu.cn>
2020-08-19 02:29:26 +0800

    backend: update template for static files

--- backend/course/app/config/secure_template.py
+++ backend/course/app/config/secure_template.py
@@ -1,9 +1,15 @@
 """
 This is the template for secure.py, please created your own secure.py this way.
 """
+import os
+from os.path import join
+
 SQLALCHEMY_DATABASE_URI = \
     'mysql+pymysql://root:123456@localhost/name'
 
 # SECRET_KEY = 'Lin Huancheng wudi'
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+UPLOAD_FOLDER = join(os.path.abspath(os.path.join(os.getcwd())), "static/uploads")
+ALLOWED_EXTENSIONS = ['*']
+MAX_CONTENT_LENGTH = 1000 * 1024 * 1024  # 1000mb
+
 JWT_SECRET_KEY = 'Lin Huancheng zuishuai'
