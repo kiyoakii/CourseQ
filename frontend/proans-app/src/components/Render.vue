6b04b5af916c8c892c80c45816ca1f6a993d0a4b
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-08-18 17:09:38 +0800

    Add cdn for hljs & markdown css

--- frontend/proans-app/src/components/Render.vue
+++ frontend/proans-app/src/components/Render.vue
@@ -9,17 +9,12 @@ import { mavonEditor } from 'mavon-editor';
 
 export default {
   name: 'Render',
-  data() {
-    return {
-      rawHtml: '',
-    };
-  },
   props: {
     markdown: String,
   },
-  watch: {
-    markdown() {
-      this.rawHtml = mavonEditor.mixins[0].data().markdownIt.render(this.markdown);
+  computed: {
+    rawHtml() {
+      return mavonEditor.getMarkdownIt().render(this.markdown);
     },
   },
 };
