2e6e1399b56fc13c0048a87b4a426b900fb03a62
biuren <biuren@mail.ustc.edu.cn>
2020-08-19 02:27:19 +0800

    backend: storage static files

--- backend/course/app/models/resource.py
+++ backend/course/app/models/resource.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, LargeBinary
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import reconstructor
 
 from app.models.base import Base
@@ -6,11 +6,17 @@ from app.models.base import Base
 
 class CourseResource(Base):
     id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    data = Column(LargeBinary)
+    description = Column(String(127))
+    filename = Column(String(127))
+    file = Column(String(127))
     course_id = Column(Integer, ForeignKey('course.cid'))
 
     @reconstructor
     def __init__(self):
         super().__init__()
-        self.fields = ['id', 'name']
+        self.fields = ['id', 'filename', 'url']
+
+    @property
+    def url(self):
+        # todo
+        return 'http://维修中/' + self.file
