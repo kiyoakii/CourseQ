50ca025f9316391519b59551d580c2e37d1ab3a8
zfming <zfmfengming@mail.ustc.edu.cn>
2020-08-19 21:24:05 +0800

    Add the function to upload image for Editor

--- frontend/proans-app/src/components/Editor.vue
+++ frontend/proans-app/src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="editor">
+  <div>
     <mavon-editor @change="change" v-model="contentCopy" :toolbars="toolbars"
-      :boxShadow="false" :tabSize="4" style="border: 1px solid #EBEEF5;"></mavon-editor>
+      :boxShadow="false" :tabSize="4" ref=md @imgAdd="$imgAdd"></mavon-editor>
   </div>
 </template>
 
@@ -15,8 +15,8 @@ export default {
         italic: true, // 斜体
         header: true, // 标题
         underline: true, // 下划线
-        // strikethrough: true, // 中划线
-        // mark: true, // 标记
+        strikethrough: true, // 中划线
+        mark: true, // 标记
         superscript: true, // 上角标
         subscript: true, // 下角标
         quote: true, // 引用
@@ -27,9 +27,9 @@ export default {
         code: true, // code
         table: true, // 表格
         fullscreen: true, // 全屏编辑
-        // help: true, // 帮助
-        // undo: true, // 上一步
-        // redo: true, // 下一步
+        help: true, // 帮助
+        undo: true, // 上一步
+        redo: true, // 下一步
         subfield: true, // 单双栏模式
         preview: true, // 预览
       },
@@ -47,6 +47,26 @@ export default {
     change(val) {
       this.$emit('change', val);
     },
+    $imgAdd(pos, $file) {
+      // 第一步.将图片上传到服务器.
+      const formdata = new FormData();
+      formdata.append('file', $file);
+      this.axios({
+        url: '/api/v1/photos',
+        method: 'post',
+        data: formdata,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }).then((res) => {
+        // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+        /**
+        * $vm 指为mavonEditor实例，可以通过如下两种方式获取
+        * 1. 通过引入对象获取: `import {mavonEditor} from ...` 等方式引入后，`$vm`为`mavonEditor`
+        * 2. 通过$refs获取: html声明ref : `<mavon-editor ref=md ></mavon-editor>，`$vm`为 `this.$refs.md`
+        */
+        // console.log(res);
+        this.$refs.md.$img2Url(pos, res.data.url);
+      });
+    },
   },
   watch: {
     content() {
@@ -55,14 +75,6 @@ export default {
   },
 };
 </script>
-
-<style>
-
-.editor .v-note-show .v-show-content.scroll-style.scroll-style-border-radius {
-  background-color: #fefefe !important;
-}
-
-</style>
 
 <style scoped>
 .footer {
