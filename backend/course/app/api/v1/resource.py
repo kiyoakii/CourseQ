fbbe1dead8c12fb37dc74b3f3a81f01ff215d62d
biuren <biuren@mail.ustc.edu.cn>
2020-08-20 01:51:54 +0800

    backend: use file operation in libs

--- backend/course/app/api/v1/resource.py
+++ backend/course/app/api/v1/resource.py
@@ -1,11 +1,7 @@
-import binascii
-import os
+from flask import request, jsonify
 
-from flask import request
-from werkzeug.utils import secure_filename
-
-from app.config.secure import UPLOAD_FOLDER
-from app.libs.error_code import Success, ParameterException, DeleteSuccess
+from app.libs.error_code import Success, DeleteSuccess
+from app.libs.file import delete_file, update_file
 from app.libs.redprint import Redprint
 from app.models import CourseResource
 from app.models.base import db
@@ -14,35 +10,30 @@ from app.validators.forms import ResourceForm
 api = Redprint('resource')
 
 
-
+@api.route('/<int:fid>', methods=['GET'])
+def get_resource(fid):
+    resource = CourseResource.query.get_or_404(fid)
+    return jsonify(resource)
 
 
 @api.route('/<int:fid>', methods=['PATCH'])
-def update_file(fid):
+def update_resource(fid):
     resource = CourseResource.query.get_or_404(fid)
     form = ResourceForm().validate_for_api()
-    file = request.files['file']
-    if file.filename == '':
-        return ParameterException
-
     with db.auto_commit():
-        if file:
-            filename = secure_filename(file.filename)
-            random_filename = str(binascii.b2a_hex(os.urandom(15)), encoding='UTF-8')
-            print(os.path.join(UPLOAD_FOLDER, random_filename))
-            file.save(os.path.join(UPLOAD_FOLDER, random_filename))
-            os.remove(os.path.join(UPLOAD_FOLDER, resource.file))
-            resource.filename = filename
-            resource.file = random_filename
+        if 'file' in request.files:
+            file = request.files['file']
+            if file and file.filename != '':
+                update_file(file, resource)
         if form.description.data:
             resource.description = form.description.data
     return Success()
 
 
 @api.route('/<int:fid>', methods=['DELETE'])
-def delete_file(fid):
+def delete_resource(fid):
     resource = CourseResource.query.get_or_404(fid)
     with db.auto_commit():
-        os.remove(os.path.join(UPLOAD_FOLDER, resource.file))
+        delete_file(resource)
         db.session.delete(resource)
     return DeleteSuccess()
