f657678fb20f02d91e1740b0aa3e4310a0b10c27
biuren <biuren@mail.ustc.edu.cn>
2020-08-25 15:23:00 +0800

    backend: add super_get_token for testing

--- backend/course/app/api/v1/token.py
+++ backend/course/app/api/v1/token.py
@@ -12,10 +12,18 @@ def get_token():
     ticket = request.args.get("ticket")
     service = request.args.get("service")
     user = User.verify(ticket, service)
-    # user = {
-    #     'uid':'000',
-    #     'gid':'0000000387',
-    #     'scope': 'StudentScope'
-    # }
     access_token = create_access_token(identity=user)
     return jsonify({'access_token': access_token, 'registered': user['scope'] != 'Scope', 'gid': user['gid']})
+
+
+@api.route('/<string:gid>', methods=['GET'])
+def super_get_token(gid):
+    user = User.query.filter_by(gid=gid).first()
+    scope = User.assign_scope(user)
+    identity = {
+        'uid': '0000000000',
+        'gid': gid,
+        'scope': scope
+    }
+    access_token = create_access_token(identity=identity)
+    return jsonify({'access_token': access_token, 'registered': identity['scope'] != 'Scope', 'gid': identity['gid']})
