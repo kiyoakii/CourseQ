16317a35c6c8cea2c033f9ef2cf12d41e24033cc
chenlighten <55092342+chenlighten@users.noreply.github.com>
2020-08-26 07:02:56 +0800

    Add test files

--- backend/course/app/tests/test_course.py
+++ backend/course/app/tests/test_course.py
@@ -1,35 +1,67 @@
 import pytest
-from course import app
 import json
-from ..models.base import db
-from app.models.course import Course
+from ..models.course import Course
+from app.tests.base import client
+from app.tests.base import get_test_token
 
 
-@pytest.fixture
-def client():
-    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['TEST_DATABASE_URI']
-    db.init_app(app)
-    with app.test_client() as client:
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
-        yield client
-
-
-def test_courses_create(client):
+def create_courses(client):
     data = {
-        "intro": "hi",
-        "name_en": "jisuanke",
-        "name_zh": "软件工程",
-        "pre_course": "ads",
-        "semester": "ad",
-        "textbooks": "dcx",
+        "intro": "test_intro",
+        "name_en": "test_name_en",
+        "name_zh": "test_name_zh",
+        "pre_course": "test_pre_course",
+        "semester": "test_semester",
+        "textbooks": "test_textbooks",
+        "series": "test_series"
     }
-    headers = {'Content-Type': 'application/json'}
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(get_test_token(client))}
     response = client.post('/v1/courses', data=json.dumps(data), headers=headers)
-    assert response.status == '201 CREATED'
-    assert Course.query.filter_by(cid=2).first().name_en == 'jisuanke'
-    assert Course.query.filter_by(cid=2).first().name_zh == '软件工程'
-    assert Course.query.filter_by(cid=2).first().pre_course == 'ads'
-    assert Course.query.filter_by(cid=2).first().semester == 'ad'
-    assert Course.query.filter_by(cid=2).first().textbooks == 'dcx'
+    return response
+
+
+def test_create_courses(client):
+    response = create_courses(client)
+    assert response.status == '200 OK', response.data
+    assert Course.query.filter_by(cid=1).first().name_en == 'test_name_en'
+    assert Course.query.filter_by(cid=1).first().name_zh == 'test_name_zh'
+    assert Course.query.filter_by(cid=1).first().pre_course == 'test_pre_course'
+    assert Course.query.filter_by(cid=1).first().semester == 'test_semester'
+    assert Course.query.filter_by(cid=1).first().textbooks == 'test_textbooks'
+    assert Course.query.filter_by(cid=1).first().series == 'test_series'
+
+
+def test_get_course(client):
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(get_test_token(client))}
+    response = client.get('/v1/courses/1', headers=headers)
+    res_json = json.loads(response.data)
+    assert response.status == '200 OK', res_json
+    assert res_json['intro'] == 'test_intro'
+    assert res_json['name_en'] == 'test_name_en'
+    assert res_json['name_zh'] == 'test_name_zh'
+    assert res_json['pre_course'] == 'test_pre_course'
+    assert res_json['semester'] == 'test_semester'
+
+
+def test_update_course(client):
+    data = {
+        "intro": "test_update_intro",
+        "name_en": "test_update_name_en",
+        "name_zh": "test_update_name_zh",
+        "pre_course": "test_update_pre_course",
+        "semester": "test_update_semester",
+        "textbooks": "test_update_textbooks",
+        "series": "test_update_series"
+    }
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(get_test_token(client))}
+    response = client.put('/v1/courses/1', data=json.dumps(data), headers=headers)
+    assert response.status == '200 OK', response.data
+    assert Course.query.filter_by(cid=1).first().name_en == 'test_update_name_en'
+    assert Course.query.filter_by(cid=1).first().name_zh == 'test_update_name_zh'
+
+
+def test_delete_course(client):
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(get_test_token(client))}
+    response = client.delete('/v1/courses/1', headers=headers)
+    assert response.status == '204 NO CONTENT', response.data
+    assert not Course.query.filter_by(cid=1).first()
