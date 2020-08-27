d175a3acf89485a888a23b7350cf9f3ddb6b6c52
biuren <biuren@mail.ustc.edu.cn>
2020-08-28 02:56:42 +0800

    backend: remove useless v2 api

--- backend/course/app/api/v2/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.api.v2 import course
-
-
-# from app.api.v2 import user, token, course, resource, answer, question, discussion, topic_answer, schedule, assignment
-
-def create_api_v2(api):
-    api.add_resource(course.CourseViews, '/v2/courses/<pk>', '/v2/courses')
