7e23a783413ef13b1cb581a9c152eede981e4833
HD <wangshunhong@mail.ustc.edu.cn>
2020-09-02 17:22:57 +0800

    fix: add login router & limit the permission

---
+++ frontend/admin-app/src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span>您无权访问管理系统</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  mounted() {
+    console.log(this.$store.state.gid);
+    if (this.$store.state.gid) {
+      if (this.$route.path.includes('teacher')) {
+        this.$router.push({
+          path: '/teacher/',
+          name: 'TeacherView',
+          params: {
+            tid: this.$store.state.gid,
+          },
+        });
+      } else {
+        this.$router.push({
+          path: '/admin/',
+          name: 'AdminView',
+          params: {
+            aid: this.$store.state.gid,
+          },
+        });
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
