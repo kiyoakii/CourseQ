963023a7a4f2c3992cbf537ed1eba2bffd9c5bfd
HD <wangshunhong@mail.ustc.edu.cn>
2020-09-03 01:27:51 +0800

    fix: fix auth limit

--- frontend/admin-app/src/views/LoginView.vue
+++ frontend/admin-app/src/views/LoginView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <span>您无权访问管理系统!</span>
+    <span>您的身份是{{ $store.state.auth }}，您无权访问管理系统!</span>
+    <button @click="commandHandler('logout')">点击登出</button>
   </div>
 </template>
 
@@ -13,7 +14,7 @@ export default {
       if (this.$route.path.includes('teacher')
       && this.$store.state.auth === '教师') {
         this.$router.push({
-          path: '/teacher/',
+          path: '/teacher',
           name: 'TeacherView',
           params: {
             tid: this.$store.state.gid,
@@ -21,7 +22,7 @@ export default {
         });
       } else if (this.$store.state.auth === '管理员') {
         this.$router.push({
-          path: '/admin/',
+          path: '/admin',
           name: 'AdminView',
           params: {
             aid: this.$store.state.gid,
@@ -29,6 +30,36 @@ export default {
         });
       }
     }
+  },
+  methods: {
+    commandHandler(command) {
+      if (command === 'logout') {
+        this.$confirm('确认登出, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '登出成功!',
+          });
+          this.$store.commit('setToken', '');
+          const currentUrl = window.location.href;
+          const appname = currentUrl.slice(0, currentUrl.indexOf('#'));
+          // const hashparam = currentUrl.match(/\/teacher\/\d*\//g);
+          const hashparam = 'teacher/';
+          const serviceUrl = `${appname}?hashparam=${hashparam}`;
+          window.location.href = `http://passport.ustc.edu.cn/logout?service=${encodeURIComponent(serviceUrl)}`;
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消登出',
+          });
+        });
+      } else if (command === 'profile') {
+        this.dialogVisible = true;
+      }
+    },
   },
 };
 </script>
